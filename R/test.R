be4c0590b7e64f7c8ceaa7827cd69e76980ab77c
rafap_IpeaDesktop <rafa.pereira.br@gmail.com>
2019-04-25 17:00:35 -0300

    tests

--- R/test.R
+++ R/test.R
@@ -14,7 +14,16 @@ plot(a)
 
 
 
-### 1. read_uf -------------------------
+### 2. read_municipio -------------------------
 
 a <- read_municipio(cod_mun=1200179)
+plot(a)
+
+
+
+
+
+### 3. read_mesorregiao -------------------------
+
+a <- read_mesorregiao(cod_mun=1200179)
 plot(a)
