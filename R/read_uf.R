fc5fbc071192ae3faf9afb577451fc7620f537c6
rafap_IpeaDesktop <rafa.pereira.br@gmail.com>
2019-04-25 15:00:41 -0300

    replace 'ano' with 'year'

--- R/read_uf.R
+++ R/read_uf.R
@@ -10,16 +10,16 @@ root_dir <- "L:\\\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\uf"
 
 #### Função de Leitura para os shapes da UF ####
 
-read_uf <- function(ano=NULL, cod_uf=NULL, name_uf=NULL){
+read_uf <- function(year=NULL, cod_uf=NULL, name_uf=NULL){
   
   # Test year input
-  if(is.null(ano)){ #testa se os parametros são nulos;
-    ano <- str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+") %>% max()
-    cat("Using data from latest year available:", ano)
+  if(is.null(year)){ #testa se os parametros são nulos;
+    year <- str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+") %>% max()
+    cat("Using data from latest year available:", year)
     } else {
-      # testa se o ano existe;
-      if(!(ano %in% str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"))){
-        stop(paste0("Error: Invalid Value to argument ano. It must be be one of the following: ", paste(str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"), collapse = " ")))
+      # testa se o year existe;
+      if(!(year %in% str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"))){
+        stop(paste0("Error: Invalid Value to argument year. It must be be one of the following: ", paste(str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"), collapse = " ")))
       }
     }
   
@@ -29,12 +29,12 @@ read_uf <- function(ano=NULL, cod_uf=NULL, name_uf=NULL){
     }
     else
       {
-      if( !(cod_uf %in% substr(list.files(paste0(root_dir, "\\UF_", ano)), start =  1, stop = 2))){
+      if( !(cod_uf %in% substr(list.files(paste0(root_dir, "\\UF_", year)), start =  1, stop = 2))){
         stop("Error: Invalid value to argument cod_uf.") 
       }
           }
       
-      shape <- readRDS(paste0(root_dir, "\\UF_", ano, "\\", substr(x = cod_uf, 1, 2), "UF.rds")) 
+      shape <- readRDS(paste0(root_dir, "\\UF_", year, "\\", substr(x = cod_uf, 1, 2), "UF.rds")) 
       return(shape)
 
 }
