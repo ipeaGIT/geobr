1da12e063d0ea612a6a5ce0febd089152a84463c
rafap_IpeaDesktop <rafa.pereira.br@gmail.com>
2019-04-25 16:54:41 -0300

    script for running tests

---
+++ R/test.R
@@ -0,0 +1,20 @@
+#### testing functions of mapsbrazil
+
+
+
+
+
+### 1. read_uf -------------------------
+
+
+
+# 1.1 Allow for uf 
+a <- read_uf(cod_uf = "SP")
+plot(a)
+
+
+
+### 1. read_uf -------------------------
+
+a <- read_municipio(cod_mun=1200179)
+plot(a)
