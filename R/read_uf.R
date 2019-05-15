c78d29ee492ad1b6d653bdb073cac3db4b8e6a3f
rodrigoarruda14 <rodrigo.arruda14@gmail.com>
2019-05-15 10:09:03 -0300

    Adição da possibilidade de leitura por sigla_uf;

--- R/read_uf.R
+++ R/read_uf.R
@@ -3,19 +3,22 @@ library(stringr)
 library(sf)
 library(dplyr)
 library(magrittr)
+library(readr)
 
 
 # Diretorio raiz
-root_dir <- "L:\\\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\uf"
+root_dir <- "L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/uf"
 
-#### Função de Leitura para os shapes da UF ####
+source("L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/sg.txt")
+
+#### Função de Leitura para os shapes da UF ----
 
 read_uf <- function(year=NULL, cod_uf=NULL, name_uf=NULL){
   
   # Test year input
   if(is.null(year)){
     year <- str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+") %>% max()
-    cat("Using data from latest year available:", year)
+    cat("Using data from latest year available:", year, "\n")
     } else {
       # test if year input exists
       if(!(year %in% str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"))){
@@ -24,22 +27,30 @@ read_uf <- function(year=NULL, cod_uf=NULL, name_uf=NULL){
     }
   
   # Test UF input
-  if(is.null(cod_uf)){ # if NULL read the entire country
+  if(is.null(cod_uf)){ # if NULL, read the entire country
+    cat("Using data from entire country \n")
     files <- list.files(paste0(root_dir, "\\UF_", year), full.names=T)
     files <- lapply(X=files, FUN= readRDS)
     shape <- do.call('rbind', files)
     return(shape)
-}
-    else
-      {
+  } else if(is.numeric(cod_uf)==TRUE){
+  
       if( !(cod_uf %in% substr(list.files(paste0(root_dir, "\\UF_", year)), start =  1, stop = 2))){
         stop("Error: Invalid value to argument cod_uf.") 
+      } else{
+        shape <- readRDS(paste0(root_dir, "\\UF_", year, "\\", substr(x = cod_uf, 1, 2), "UF.rds")) 
+        return(shape)
       }
-          }
+    
+  } else if(toupper(cod_uf) %in% sg){
+    source("L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/read_tab_sg.R")
+    cod_uf <- read_tab(cod_uf)
+    shape <- readRDS(paste0(root_dir, "\\UF_", year, "\\", substr(x = cod_uf, 1, 2), "UF.rds")) 
+    return(shape)
+  }  else{
+    stop("Error: Invalid value to argument cod_uf.")
+    }
       
-      shape <- readRDS(paste0(root_dir, "\\UF_", year, "\\", substr(x = cod_uf, 1, 2), "UF.rds")) 
-      return(shape)
-
 }
 
 
