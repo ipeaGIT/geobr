a0c33be307419a2de47e33c6ce999aca8a993b14
Pauloaraujoo <hfa.paulo@gmail.com>
2019-05-16 11:54:02 -0300

    Inserindo opção de ano mais recente e de pegar o Brasil todo.

--- R/read_microregiao.R
+++ R/read_microregiao.R
@@ -3,36 +3,48 @@ library(stringr)
 library(sf)
 library(dplyr)
 library(magrittr)
+library(readr)
 
 
 # Diretorio raiz
-root_dir <- "L:\\\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\micro_regiao"
+root_dir <- "L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/micro_regiao"
 
 
-#### Função de Leitura para os shapes da microrregião ####
+#### Função de Leitura para os shapes da microregiao ----
 
-read_microrregiao <- function(ano=NULL, cod_micro=NULL){
+read_microregiao <- function(year=NULL, cod_micro=NULL){
   
-  if(is.null(ano) || is.null(cod_micro)){ #testa se os parametros são nulos;
-    stop("Error: Invalid value to argument ano or argument cod_micro.") 
-  }
-  if(ano %in% str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+")){ #testa se o ano existe;
-    if(substr(x = cod_micro, 1, 2) %in% substr(list.files(paste0(root_dir, "\\MI_", ano)), 1, 2)){ #testa se o estado existe;
-      
-      shape <- readRDS(paste0(root_dir, "\\MI_", ano, "\\", substr(x = cod_micro, 1, 2), "MI.rds")) 
-      
-      if(cod_micro %in% shape$cod_micro){ #testa se a microrregião existe;
-        shape %<>% filter(cod_micro==cod_micro)
-        return(shape)
-      } else{
-        stop("Error: Invalid Value to argument cod_micro.")
-      }
-    } else{
-      stop("Error: Invalid Value to argument cod_micro.")
+  # Test year input
+  if(is.null(year)){
+    year <- str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+") %>% max()
+    cat("Using data from latest year available:", year, "\n")
+  } else {
+    # test if year input exists
+    if(!(year %in% str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"))){
+      stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ", paste(str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"), collapse = " ")))
     }
-  } else{
-    stop(paste0("Error: Invalid Value to argument ano. It must be one of the following: ", paste(str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"), collapse = " ")))
   }
+  
+  # Test micro input
+  if(is.null(cod_micro)){ # if NULL, read the entire country
+    cat("Using data from entire country \n")
+    files <- list.files(paste0(root_dir, "\\MI_", year), full.names=T)
+    files <- lapply(X=files, FUN= readRDS)
+    shape <- do.call('rbind', files)
+    return(shape)
+  } 
+  
+  if( !(substr(x = cod_micro, 1, 2) %in% substr(list.files(paste0(root_dir, "\\MI_", year)), start =  1, stop = 2))){
+    stop("Error: Invalid value to argument cod_micro.") 
+    
+  } else{
+    shape <- readRDS(paste0(root_dir, "\\MI_", year, "\\", substr(x = cod_micro, 1, 2), "MI.rds")) 
+    if(cod_micro %in% shape$cod_micro){ #testa se a microregiao existe;
+      cod_micro_auxiliar <- cod_micro
+      shape %<>% filter(cod_micro==cod_micro_auxiliar)
+      return(shape)
+      
+    } else{stop("Error: Invalid Value to argument cod_micro.")}
+  }
+  
 }
-
-
