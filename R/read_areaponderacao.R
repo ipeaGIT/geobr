7462546c187b0594afed08187b3cfa8a991d6633
IgorNascimento <igor.ferreira.n@gmail.com>
2019-05-18 17:41:56 -0300

    Iniciando documentação das funções.
    Editando arquivo DESCRIPTION
    Gerando manual

--- R/read_areaponderacao.R
+++ R/read_areaponderacao.R
@@ -1,21 +1,48 @@
 # Funcao de leitura -------------------------------------------------------
 
+#' Title
+#'
+#' @param CODE
+#' @param year
+#'
+#' @return
+#' @export
+#'
+#' @examples \dontrun{
+#' # Exemplos
+#'dados <- read_areaponderacao(year=2010)
+#'dados <- read_areaponderacao(3500000,2010)
+#'dados <- read_areaponderacao(123,2010)
+#'dados <- read_areaponderacao("df",2010)
+#'dados <- read_areaponderacao(1302603,2010)
+#'dados <- read_areaponderacao(35)
+#'dados <- read_areaponderacao(14,2010)
+#'dados <- read_areaponderacao()
+#'
+#'# mapa
+#'library(mapview)
+#'mapview(dados)
+#' }
+#'
+#'
+#'
+#'
 read_areaponderacao <- function(CODE = NULL,year = NULL){
   ## Pacotes
   library(stringr)
   library(sf)
   library(dplyr)
   library(magrittr)
-  
-  # definindo diretorio de download dos arquivos 
+
+  # definindo diretorio de download dos arquivos
   dir.proj <- paste0("L:\\\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\area_ponderacao")
-  
+
   library(assertthat)
   # verificando se a pessoa entrou com uma string
   if( is.string(CODE)) {
-    stop(paste0("Invalid value to UF or MUN")) 
+    stop(paste0("Invalid value to UF or MUN"))
   }
-  
+
   if(is.null(year)){
     year <- str_extract(list.files(dir.proj), pattern = "[0-9]+") %>% max()
     cat("Using data from latest year available:", year)
@@ -25,7 +52,7 @@ read_areaponderacao <- function(CODE = NULL,year = NULL){
       stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ", paste(str_extract(list.files(dir.proj), pattern = "[0-9]+")) , collapse = " "))
     }
   }
-  
+
   # CODE NULL
   if( is.null(CODE)) {
     #ler brasil
@@ -40,42 +67,27 @@ read_areaponderacao <- function(CODE = NULL,year = NULL){
 
   if(nchar(CODE) == 2) {
     if(!(CODE %in% as.numeric(list.files(paste(dir.proj,year,sep="/"),pattern = "^\\d"))) ){
-      stop(paste0("Invalid value to UF. Must be one of the following: 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 52, 53")) 
+      stop(paste0("Invalid value to UF. Must be one of the following: 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 52, 53"))
     }
-    
+
     if (length(list.files(paste(dir.proj,year,CODE,sep="/")))==0){stop(paste0("UF has no weighting area."))}
-    
+
     files <- list.files(paste(dir.proj,year,CODE,sep="/"),full.names = T)
     files <- lapply(X=files, FUN= readRDS)
     files <- lapply(X=files, FUN= as.data.frame)
     shape <- do.call('rbind', files)
     shape <- st_sf(shape)
     return(shape)
-   
-    } 
-  
+
+    }
+
   if(nchar(CODE) == 7) {
     if( !(CODE %in% substr(list.files(paste(dir.proj, year,substr(CODE,1,2),sep="\\")), start =  1, stop = 7))){
-      stop(paste0("Invalid value to MUN.")) 
+      stop(paste0("Invalid value to MUN."))
     }
     return(readRDS(paste(paste(dir.proj,year,substr(CODE,1,2),CODE,sep="\\"),"_areaponderacao_",year,".rds",sep="")))
   }
-  
-  stop(paste0("Invalid value to CODE.")) 
+
+  stop(paste0("Invalid value to CODE."))
 }
-
-
-# Exemplos
-dados <- read_areaponderacao(year=2010)
-dados <- read_areaponderacao(3500000,2010)
-dados <- read_areaponderacao(123,2010)
-dados <- read_areaponderacao("df",2010)
-dados <- read_areaponderacao(1302603,2010)
-dados <- read_areaponderacao(35)
-dados <- read_areaponderacao(14,2010)
-dados <- read_areaponderacao()
-
-# mapa
-library(mapview)
-mapview(dados)
 
--- R/read_uf.R
+++ R/read_uf.R
@@ -10,12 +10,29 @@ library(readr)
 root_dir <- "L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/uf"
 
 # Leitura das siglas dos estados
-source("L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/sg.txt")
+# source("L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/sg.txt")
 
 #### Função de Leitura para os shapes da UF ----
 
+#' Download shape files of state
+#'
+#' @param year the year of the data download (defaults to 2010)
+#' @param cod_uf 2-digit state code. If not informed, all states will be loaded.
+#' @export
+#' @family general area functions
+#' @examples \dontrun{
+#'
+#' library(geobr)
+#'
+#' # Read specific municipality at a given year
+#'   mun <- read_municipio(cod_uf=12, year=2017)
+#'
+#'# Read al lstate at a given year
+#'   mun <- read_municipio(cod_mun=12, year=2010)
+#'
+#'}
 read_uf <- function(year=NULL, cod_uf=NULL){
-  
+
   # Test year input
   if(is.null(year)){
     year <- str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+") %>% max()
@@ -26,7 +43,7 @@ read_uf <- function(year=NULL, cod_uf=NULL){
         stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ", paste(str_extract(list.files(root_dir, pattern = ".*\\_"), pattern = "[0-9]+"), collapse = " ")))
       }
     }
-  
+
   # Test UF input
   if(is.null(cod_uf)){ # if NULL, read the entire country
     cat("Using data from entire country \n")
@@ -35,23 +52,23 @@ read_uf <- function(year=NULL, cod_uf=NULL){
     shape <- do.call('rbind', files)
     return(shape)
   } else if(is.numeric(cod_uf)==TRUE){
-  
+
       if( !(cod_uf %in% substr(list.files(paste0(root_dir, "\\UF_", year)), start =  1, stop = 2))){
-        stop("Error: Invalid value to argument cod_uf.") 
+        stop("Error: Invalid value to argument cod_uf.")
       } else{
-        shape <- readRDS(paste0(root_dir, "/UF_", year, "/", substr(x = cod_uf, 1, 2), "UF.rds")) 
+        shape <- readRDS(paste0(root_dir, "/UF_", year, "/", substr(x = cod_uf, 1, 2), "UF.rds"))
         return(shape)
       }
-    
+
   } else if(toupper(cod_uf) %in% sg){
     source("L:/# DIRUR #/ASMEQ/pacoteR_shapefilesBR/data/read_tab_sg.R")
     cod_uf <- read_tab(cod_uf)
-    shape <- readRDS(paste0(root_dir, "/UF_", year, "/", substr(x = cod_uf, 1, 2), "UF.rds")) 
+    shape <- readRDS(paste0(root_dir, "/UF_", year, "/", substr(x = cod_uf, 1, 2), "UF.rds"))
     return(shape)
   }  else{
     stop("Error: Invalid value to argument cod_uf.")
     }
-      
+
 }
 
 
