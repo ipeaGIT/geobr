fb7a91f69702d4c6f65d41f11a7c7e7e03448091
rafaepereirabr <rafa.pereira.br@gmail.com>
2019-06-08 22:28:01 -0300

    adjust correspondence_table_stategrid data and fun

--- R/read_statistical_grid.R
+++ R/read_statistical_grid.R
@@ -29,17 +29,8 @@ read_statistical_grid <- function(year=NULL, cod_grid=NULL){
   }
   
   
-# read correspondence table
-  
-  # # Change this line! to load data from within the package `data` directory
-  # setwd("R:/Dropbox/git_projects/geobr")
-  # corresptb <- readr::read_rds("./data/correspondence_table_statgrid.rds") 
-  # 
-  # save(corresptb, file="./data/correspondence_table_statgrid.RData") 
-  # 
-  # load(file="./data/correspondence_table_statgrid.RData")
-  # get reference data
-  data("corresptb", envir=environment())
+# load correspondence table
+  data("correspondence_table_stategrid", envir=environment())
   
   
   
