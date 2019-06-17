ddeee99d6a7325aed7df2dd4114acad2d85adc02
rafap_IpeaDesktop <rafa.pereira.br@gmail.com>
2019-06-17 18:44:32 -0300

    R script of brazil_2010 data, v1 incomplete

---
+++ R/brazil_2010.R
@@ -0,0 +1,30 @@
+#' Spatial dataset sf with codes for Brazilian municipalities in 2010
+#'
+#' @title Spatial dataset sf with codes for Brazilian municipalities in 2010
+#' @description Built-in dataset for use with the \code{lookup_code} function.
+#'              To access the data directly, issue the command \code{data("brazil_2010")}.
+#'
+#' \itemize{
+#'   \item \code{cod_muni}: Municipality IBGE code (7-digit, 0-padded, numeric)
+#'   \item \code{name_muni}: Title-case name of municipality
+#'   \item \code{cod_micro}: 
+#'   \item \code{name_micro}: Title-case name of micro region
+#'   \item \code{cod_meso}: 
+#'   \item \code{name_meso}: Title-case name of meseo region
+#'   \item \code{cod_state}: 
+#'   \item \code{name_state}: Title-case name of state
+#'   \item \code{abbrev_state}: 
+#'   \item \code{cod_region}: 
+#'   \item \code{name_region}: Title-case name of region
+#'   \item \code{geometry}: 
+
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name brazil_2010
+#'
+#' @usage data(brazil_2010)
+#' @note Last updated 2019-06-17
+#' @format A data frame with 3,236 rows and 5 variables
+"brazil_2010"
