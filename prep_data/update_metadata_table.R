27d926eb4dc482d8cdcd3643b2dc35f3913cc3e3
CaioNG <caio.goncalves@ipea.gov.br>
2019-07-16 17:12:19 -0300

    testes for rural and urban regions

--- prep_data/update_metadata_table.R
+++ prep_data/update_metadata_table.R
@@ -7,10 +7,11 @@
 
 # list all data files available in the geobr package
   geo=list.files("//storage3/geobr/data")
-  
+  # geo<-"setor_censitario"
 # populate the metadata table
   for (a in geo) {
     ano=list.files(paste("//storage3/geobr/data",a,sep="/"))
+    ano=ano[!grepl("Urbano|Rural", ano)]
     for (b in ano) {
       estado=list.files(paste("//storage3/geobr/data",a,b,sep="/"))
       for (c in estado) {
@@ -19,36 +20,61 @@
     }
   }
 
+  for (a in geo) {
+    ano=list.files(paste("//storage3/geobr/data",a,sep="/"))
+    ano=ano[grepl("Urbano", ano)]
+    ano=list.files(paste("//storage3/geobr/data",a,ano,sep="/"))
+    for (b in ano) {
+      estado=list.files(paste("//storage3/geobr/data",a,"Urbano",b,sep="/"))
+      for (c in estado) {
+        metadata[nrow(metadata) + 1,] = list(a,b,paste0("U",substr(c, 1, 2)),paste("http://www.ipea.gov.br/geobr/data",a,"Urbano",b,c,sep="/"))
+      }
+    }
+  }
+  
+  for (a in geo) {
+    ano=list.files(paste("//storage3/geobr/data",a,sep="/"))
+    ano=ano[grepl("Rural", ano)]
+    ano=list.files(paste("//storage3/geobr/data",a,ano,sep="/"))
+    for (b in ano) {
+      estado=list.files(paste("//storage3/geobr/data",a,"Rural",b,sep="/"))
+      for (c in estado) {
+        metadata[nrow(metadata) + 1,] = list(a,b,paste0("R",substr(c, 1, 2)),paste("http://www.ipea.gov.br/geobr/data",a,"Rural",b,c,sep="/"))
+      }
+    }
+  }
+  
+  
 # get code abbreviations
   library(data.table)
   setDT(metadata)
-  metadata[ code== 11, code_abrev :=	"RO" ]
-  metadata[ code== 12, code_abrev :=	"AC" ]
-  metadata[ code== 13, code_abrev :=	"AM" ]
-  metadata[ code== 14, code_abrev :=	"RR" ]
-  metadata[ code== 15, code_abrev :=	"PA" ]
-  metadata[ code== 16, code_abrev :=	"AP" ]
-  metadata[ code== 17, code_abrev :=	"TO" ]
-  metadata[ code== 21, code_abrev :=	"MA" ]
-  metadata[ code== 22, code_abrev :=	"PI" ]
-  metadata[ code== 23, code_abrev :=	"CE" ]
-  metadata[ code== 24, code_abrev :=	"RN" ]
-  metadata[ code== 25, code_abrev :=	"PB" ]
-  metadata[ code== 26, code_abrev :=	"PE" ]
-  metadata[ code== 27, code_abrev :=	"AL" ]
-  metadata[ code== 28, code_abrev :=	"SE" ]
-  metadata[ code== 29, code_abrev :=	"BA" ]
-  metadata[ code== 31, code_abrev :=	"MG" ]
-  metadata[ code== 32, code_abrev :=	"ES" ]
-  metadata[ code== 33, code_abrev :=	"RJ" ]
-  metadata[ code== 35, code_abrev :=	"SP" ]
-  metadata[ code== 41, code_abrev :=	"PR" ]
-  metadata[ code== 42, code_abrev :=	"SC" ]
-  metadata[ code== 43, code_abrev :=	"RS" ]
-  metadata[ code== 50, code_abrev :=	"MS" ]
-  metadata[ code== 51, code_abrev :=	"MT" ]
-  metadata[ code== 52, code_abrev :=	"GO" ]
-  metadata[ code== 53, code_abrev :=	"DF" ]
+  metadata[ grepl("11", code), code_abrev :=	"RO" ]
+  metadata[ grepl("12", code), code_abrev :=	"AC" ]
+  metadata[ grepl("13", code), code_abrev :=	"AM" ]
+  metadata[ grepl("14", code), code_abrev :=	"RR" ]
+  metadata[ grepl("15", code), code_abrev :=	"PA" ]
+  metadata[ grepl("16", code), code_abrev :=	"AP" ]
+  metadata[ grepl("17", code), code_abrev :=	"TO" ]
+  metadata[ grepl("21", code), code_abrev :=	"MA" ]
+  metadata[ grepl("22", code), code_abrev :=	"PI" ]
+  metadata[ grepl("23", code), code_abrev :=	"CE" ]
+  metadata[ grepl("24", code), code_abrev :=	"RN" ]
+  metadata[ grepl("25", code), code_abrev :=	"PB" ]
+  metadata[ grepl("26", code), code_abrev :=	"PE" ]
+  metadata[ grepl("27", code), code_abrev :=	"AL" ]
+  metadata[ grepl("28", code), code_abrev :=	"SE" ]
+  metadata[ grepl("29", code), code_abrev :=	"BA" ]
+  metadata[ grepl("31", code), code_abrev :=	"MG" ]
+  metadata[ grepl("32", code), code_abrev :=	"ES" ]
+  metadata[ grepl("33", code), code_abrev :=	"RJ" ]
+  metadata[ grepl("35", code), code_abrev :=	"SP" ]
+  metadata[ grepl("41", code), code_abrev :=	"PR" ]
+  metadata[ grepl("42", code), code_abrev :=	"SC" ]
+  metadata[ grepl("43", code), code_abrev :=	"RS" ]
+  metadata[ grepl("50", code), code_abrev :=	"MS" ]
+  metadata[ grepl("51", code), code_abrev :=	"MT" ]
+  metadata[ grepl("52", code), code_abrev :=	"GO" ]
+  metadata[ grepl("53", code), code_abrev :=	"DF" ]
 
   
 # save updated metadata table
