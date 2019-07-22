3d44c7bb1355b56e4706fd98da12c06ef9be01a5
CaioNG <caio.goncalves@ipea.gov.br>
2019-07-22 19:29:26 -0300

    updating prep_census_tract
    updating update_metadata_table
    updating read_census_tract
    
    
    Former-commit-id: 011aad9eac7f91f197739dc2b4ef3d99a0599f4e

--- R/read_census_tract.R
+++ R/read_census_tract.R
@@ -25,76 +25,215 @@
 #'
 #'
 read_census_tract <- function(CODE, year = NULL, zone = "urban"){
-  ## Pacotes
-  library(stringr)
-  library(sf)
-  library(dplyr)
-  library(magrittr)
 
-  # definindo diretorio de download dos arquivos
-  dir.proj <- paste0("\\\\storage6\\usuarios\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\setor_censitario")
+  # Get metadata with data addresses
+  tempf <- file.path(tempdir(), "metadata.rds")
+  # check if metadata has already been downloaded
+  if (file.exists(tempf)) {
+    metadata <- readr::read_rds(tempf)
 
-  if (year==2000 & zone == "urban") {
-    dir.proj <- paste0("\\\\storage6\\usuarios\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\setor_censitario\\Urbano")
+  } else { # download it and save to metadata
+    httr::GET(url="http://www.ipea.gov.br/geobr/metadata/metadata.rds", httr::write_disk(tempf, overwrite = T))
+    metadata <- readr::read_rds(tempf)
+  }
+
+  # Select geo
+  temp_meta <- subset(metadata, geo=="setor_censitario")
+
+
+
+
+  # Verify year input
+  if (is.null(year)){ cat("Using data from year 2010\n")
+    temp_meta <- subset(temp_meta, year==2010)
+    year<-2010
+
+  } else if (year %in% temp_meta$year){ temp_meta <- temp_meta[temp_meta[,2] == year, ]
+
+  } else { stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ",
+                       paste(unique(temp_meta$year),collapse = " ")))
+  }
+
+
+  # Verify code_micro input
+
+  # Test if CODE input is null
+  if(is.null(CODE)){ stop("Value to argument 'CODE' cannot be NULL") }
+
+
+  if(as.numeric(year)>=2010){
+
+
+    # if CODE=="all", read the entire country
+    if(CODE=="all"){ cat("Loading data for the whole country. This might take a few minutes.\n")
+
+      # list paths of files to download
+      filesD <- as.character(temp_meta$download_path)
+
+
+      # download files
+      counter <- 0
+      lapply(X=filesD, function(X){ counter <<- counter + 1
+      print(paste("Downloading ", counter, " of ",length(filesD)," files"))
+      httr::GET(url=X, httr::progress(),
+                httr::write_disk(paste0(tempdir(),"/", unlist(lapply(strsplit(X,"/"),tail,n=1L))), overwrite = T))})
+
+
+      # read files and pile them up
+      files <- unlist(lapply(strsplit(filesD,"/"), tail, n = 1L))
+      files <- paste0(tempdir(),"/",files)
+      files <- lapply(X=files, FUN= readr::read_rds)
+      sf <- do.call('rbind', files)
+      return(sf)
+    }
+
+    else if( !(substr(x = CODE, 1, 2) %in% temp_meta$code) & !(toupper(substr(x = CODE, 1, 2)) %in% temp_meta$code_abrev)){
+
+      stop("Error: Invalid Value to argument CODE.")
+
+    } else{
+
+      # list paths of files to download
+      if (is.numeric(CODE)){ filesD <- as.character(subset(temp_meta, code==substr(CODE, 1, 2))$download_path) }
+      if (is.character(CODE)){ filesD <- as.character(subset(temp_meta, code_abrev==toupper(substr(CODE, 1, 2)))$download_path) }
+
+      # download files
+      temps <- paste0(tempdir(),"/",unlist(lapply(strsplit(filesD,"/"),tail,n=1L)))
+      httr::GET(url=filesD,  httr::progress(), httr::write_disk(temps, overwrite = T))
+
+      # read sf
+      sf <- readr::read_rds(temps)
+
+      if(nchar(CODE)==2){
+        return(sf)
+
+      } else if(CODE %in% sf$municipality_code){    # Get Municipio
+        x <- CODE
+        sf <- subset(sf, municipality_code==x)
+        return(sf)
+      } else{
+        stop("Error: Invalid Value to argument CODE.")
+      }
+    }
+
+
+  } else if (year==2000 & zone == "urban") {
+
+    temp_meta <- temp_meta[temp_meta[,3] %like% "U", ]
+
+    # if CODE=="all", read the entire country
+    if(CODE=="all"){ cat("Loading data for the whole country. This might take a few minutes.\n")
+
+      # list paths of files to download
+      filesD <- as.character(temp_meta$download_path)
+
+
+      # download files
+      counter <- 0
+      lapply(X=filesD, function(X){ counter <<- counter + 1
+      print(paste("Downloading ", counter, " of ",length(filesD)," files"))
+      httr::GET(url=X, httr::progress(),
+                httr::write_disk(paste0(tempdir(),"/", unlist(lapply(strsplit(X,"/"),tail,n=1L))), overwrite = T))})
+
+
+      # read files and pile them up
+      files <- unlist(lapply(strsplit(filesD,"/"), tail, n = 1L))
+      files <- paste0(tempdir(),"/",files)
+      files <- lapply(X=files, FUN= readr::read_rds)
+      sf <- do.call('rbind', files)
+      return(sf)
+    }
+    else if( !(paste0("U",substr(x = CODE, 1, 2)) %in% substr(temp_meta$code, 1, 3)) & !(toupper(substr(x = CODE, 1, 2)) %in% temp_meta$code_abrev)){
+
+      stop("Error: Invalid Value to argument CODE.")
+
+    } else if( (is.numeric(CODE) & nchar(CODE)==2)|(is.character(CODE) & nchar(CODE)==2) ){
+
+      if (is.numeric(CODE)) {temp_meta <- temp_meta[temp_meta[,3] %like% paste0("U",CODE), ] }
+
+      if (is.character(CODE)) {temp_meta <- temp_meta[temp_meta[,5] %like% CODE, ] }
+
+      # list paths of files to download
+      filesD <- as.character(temp_meta$download_path)
+
+
+      # download files
+      counter <- 0
+      lapply(X=filesD, function(X){ counter <<- counter + 1
+      print(paste("Downloading ", counter, " of ",length(filesD)," files"))
+      httr::GET(url=X, httr::progress(),
+                httr::write_disk(paste0(tempdir(),"/", unlist(lapply(strsplit(X,"/"),tail,n=1L))), overwrite = T))})
+
+
+      # read files and pile them up
+      files <- unlist(lapply(strsplit(filesD,"/"), tail, n = 1L))
+      files <- paste0(tempdir(),"/",files)
+      files <- lapply(X=files, FUN= readr::read_rds)
+      sf <- do.call('rbind', files)
+      return(sf)
+
+    }else if(paste0("U",CODE) %in% temp_meta$code){    # Get Municipio
+
+      filesD <- as.character(subset(temp_meta, code==paste0("U",CODE))$download_path)
+
+      temps <- paste0(tempdir(),"/",unlist(lapply(strsplit(filesD,"/"),tail,n=1L)))
+
+      httr::GET(url=filesD,  httr::progress(), httr::write_disk(temps, overwrite = T))
+
+      # read sf
+      sf <- readr::read_rds(temps)
+      return(sf)
+    } else{
+      stop("Error: Invalid Value to argument CODE.")
+    }
   } else if (year==2000 & zone == "rural") {
-    dir.proj <- paste0("\\\\storage6\\usuarios\\# DIRUR #\\ASMEQ\\pacoteR_shapefilesBR\\data\\setor_censitario\\Rural")
-  }
+    temp_meta <- temp_meta[temp_meta[,3] %like% "R", ]
+
+    # if CODE=="all", read the entire country
+    if(CODE=="all"){ cat("Loading data for the whole country. This might take a few minutes.\n")
+
+      # list paths of files to download
+      filesD <- as.character(temp_meta$download_path)
 
 
-  library(assertthat)
-  # verificando se a pessoa entrou com uma string
-  if( (is.string(CODE) & CODE != "ALL")|is.null(CODE)) {
-    stop(paste0("Invalid value to UF or MUN"))
-  }
+      # download files
+      counter <- 0
+      lapply(X=filesD, function(X){ counter <<- counter + 1
+      print(paste("Downloading ", counter, " of ",length(filesD)," files"))
+      httr::GET(url=X, httr::progress(),
+                httr::write_disk(paste0(tempdir(),"/", unlist(lapply(strsplit(X,"/"),tail,n=1L))), overwrite = T))})
 
 
-  if(is.null(year)){
-    year <- str_extract(list.files(dir.proj), pattern = "[0-9]+") %>% max()
-    cat("Using data from latest year available:", year,"\n")
-  } else {
-    # test if year input exists
-    if(!(year %in% str_extract(list.files(dir.proj), pattern = "[0-9]+"))){
-      stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ", paste(str_extract(list.files(dir.proj), pattern = "[0-9]+")) , collapse = " "))
+      # read files and pile them up
+      files <- unlist(lapply(strsplit(filesD,"/"), tail, n = 1L))
+      files <- paste0(tempdir(),"/",files)
+      files <- lapply(X=files, FUN= readr::read_rds)
+      sf <- do.call('rbind', files)
+      return(sf)
+    }else{
+
+      # list paths of files to download
+      if (is.numeric(CODE)){ filesD <- as.character(subset(temp_meta, code==paste0("R",substr(CODE, 1, 2)))$download_path) }
+      if (is.character(CODE)){ filesD <- as.character(subset(temp_meta, code_abrev==toupper(substr(CODE, 1, 2)))$download_path) }
+
+      # download files
+      temps <- paste0(tempdir(),"/",unlist(lapply(strsplit(filesD,"/"),tail,n=1L)))
+      httr::GET(url=filesD,  httr::progress(), httr::write_disk(temps, overwrite = T))
+
+      # read sf
+      sf <- readr::read_rds(temps)
+
+      if(nchar(CODE)==2){
+        return(sf)
+
+      } else if(CODE %in% sf$municipality_code){    # Get Municipio
+        x <- CODE
+        sf <- subset(sf, municipality_code==x)
+        return(sf)
+      } else{
+        stop("Error: Invalid Value to argument CODE.")
+      }
     }
-  }
-
-  if(CODE=="all"){
-    cat("Loading data for the whole country \n")
-    f <- list.files(paste0(dir.proj,"\\SC_",year),pattern = "^\\d")
-    files <- list.files(paste0(dir.proj,"\\SC_",year,"/",f),pattern = ".rds$|.RDS$",full.names = TRUE)
-    files <- lapply(X=files, FUN= readr::read_rds)
-    files <- lapply(X=files, FUN= as.data.frame)
-    shape <- do.call('rbind', files)
-    shape <- st_sf(shape)
-    return(shape)
-  }
-
-  if(nchar(CODE) == 2) {
-    if(!(CODE %in% as.numeric(list.files(paste0(dir.proj,"\\SC_",year),pattern = "^\\d"))) ){
-      stop(paste0("Invalid value to UF. Must be one of the following: 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 52, 53"))
-    }
-
-    if (length(list.files(paste0(dir.proj,"\\SC_",year,"/",CODE)))==0){stop(paste0("UF has no census sectors."))}
-
-    files <- list.files(paste0(dir.proj,"\\SC_",year,"/",CODE),pattern = ".rds$|.RDS$",full.names = TRUE)
-    files <- lapply(X=files, FUN= readr::read_rds)
-    files <- lapply(X=files, FUN= as.data.frame)
-    shape <- do.call('rbind', files)
-    shape <- st_sf(shape)
-    return(shape)
 
   }
-
-  if(nchar(CODE) == 7) {
-    if( !(CODE %in% substr(list.files(paste(dir.proj, paste0("SC_",year),substr(CODE,1,2),"municipios",sep="\\")), start =  3, stop = 9))){
-      stop(paste0("Invalid value to MUN."))
-    }
-    if (length(list.files(paste(paste(dir.proj, paste0("SC_",year),substr(CODE,1,2),"municipios\\",sep="\\"),"SC",CODE,".rds",sep="")))==1) {
-      return(readr::read_rds(paste(paste(dir.proj, paste0("SC_",year),substr(CODE,1,2),"municipios\\",sep="\\"),"SC",CODE,".rds",sep="")))
-      } else {
-    return(readr::read_rds(paste(paste(dir.proj, paste0("SC_",year),substr(CODE,1,2),"municipios\\",sep="\\"),"SC",CODE,".RDS",sep="")))
-        }
-  }
-
-  stop(paste0("Invalid value to CODE."))
 }
