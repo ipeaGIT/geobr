27e0ff12e52375d06d8165da3e52902eb6441e27
rafapereirabr <rafa.pereira.br@gmail.com>
2019-07-27 12:04:34 -0300

    updata documentation
    Former-commit-id: dfc144021adebdfc0df63ac62fb525baeba4fc8e

--- R/brazil_2010.R
+++ R/brazil_2010.R
@@ -5,19 +5,19 @@
 #'              To access the data directly, issue the command \code{data("brazil_2010")}.
 #'              Map of Brazil at scale 1:250,000, using Geodetic reference system "SIRGAS2000" and CRS(4674).
 #'              More info at <<ftp://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2010/1_leia_me/Malha_Municipal_2010.pdf>>
-#'              and <<https://ww2.ibge.gov.br/english/geociencias/geodesia/pmrg/faq.shtm 
+#'              and <<https://ww2.ibge.gov.br/english/geociencias/geodesia/pmrg/faq.shtm
 #'
 #' \itemize{
-#'   \item \code{cod_muni}: IBGE code of municipality (7-digit, numeric)
+#'   \item \code{code_muni}: IBGE code of municipality (7-digit, numeric)
 #'   \item \code{name_muni}: Title-case name of municipality (character)
-#'   \item \code{cod_micro}: IBGE code of micro region (5-digit, numeric)
+#'   \item \code{code_micro}: IBGE code of micro region (5-digit, numeric)
 #'   \item \code{name_micro}: Title-case name of micro region (character)
-#'   \item \code{cod_meso}: IBGE code of meso region (4-digit, numeric)
+#'   \item \code{code_meso}: IBGE code of meso region (4-digit, numeric)
 #'   \item \code{name_meso}: Title-case name of meso region (character)
-#'   \item \code{cod_state}: IBGE code of State (2-digit, numeric)
+#'   \item \code{code_state}: IBGE code of State (2-digit, numeric)
 #'   \item \code{name_state}: Title-case name of state (character)
 #'   \item \code{abbrev_state}: UPPER CASE abbreviation of state name (2 letters, character)
-#'   \item \code{cod_region}: IBGE code of region (1-digit, numeric)
+#'   \item \code{code_region}: IBGE code of region (1-digit, numeric)
 #'   \item \code{name_region}: Title-case name of region (character)
 #'   \item \code{geometry}: geometry info in "sfc_GEOMETRY" "sfc"
 #' }
