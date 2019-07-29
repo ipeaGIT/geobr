f9f1255f12a8b568206bf6c4b266ca42a4c0bd65
rafapereirabr <rafa.pereira.br@gmail.com>
2019-07-29 11:43:47 -0300

    fixing errors for CRAN
    Former-commit-id: 5a69febfd137f547c9f8a94e10d3bac918b49770
    Former-commit-id: 08fe2d55cc269bbd156c757a71283ee22f96a137

--- R/read_region.R
+++ R/read_region.R
@@ -13,16 +13,16 @@
 #'}
 
 read_region <- function(year=NULL){
-  
+
   # read all states
   y <- year
-  temp_sf <- read_state(code_state = "all", year = y)  
-  
+  temp_sf <- read_state(code_state = "all", year = y)
+
   # merge by Region
   temp_sf <- dplyr::select(temp_sf, 'code_region', 'geometry')
   temp_sf <- dplyr::summarize( group_by(temp_sf, code_region))
 
-  
+
   # add region names
   temp_sf <- dplyr::mutate(temp_sf, name_region = ifelse(code_region==1, 'Norte',
                                                             ifelse(code_region==2, 'Nordeste',
@@ -31,6 +31,7 @@ read_region <- function(year=NULL){
                                                                                  ifelse(code_region==5, 'Centro Oeste', NA))))))
   # reorder columns and return sf
   temp_sf <- dplyr::select(temp_sf, 'code_region', 'name_region', 'geometry')
+  sf::st_crs(temp_sf)
   return(temp_sf)
 }
 
