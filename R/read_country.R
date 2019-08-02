bb35660b1214dc56ba18b45c144efd3391da84f9
rafapereirabr <rafa.pereira.br@gmail.com>
2019-08-02 08:50:51 -0300

    Replaced \dontrun{} by \donttest{} ; and cat() by message()
    Former-commit-id: f4cd18955c07745522fe2fcbf2140b19b3acfeca
    Former-commit-id: 59539ed5f3f4f48a3bb71bc695138e6816f19de1

--- R/read_country.R
+++ R/read_country.R
@@ -3,7 +3,7 @@
 #' @param year Year of the data (defaults to 2010)
 #' @export
 #' @family general area functions
-#' @examples \dontrun{
+#' @examples \donttest{
 #'
 #' library(geobr)
 #'
@@ -13,45 +13,45 @@
 #'}
 
 read_country <- function(year=NULL){
-  
+
   # Get metadata with data addresses
   tempf <- file.path(tempdir(), "metadata.rds")
-  
+
   # check if metadata has already been downloaded
   if (file.exists(tempf)) {
     metadata <- readr::read_rds(tempf)
-    
+
   } else {
     # download it and save to metadata
     httr::GET(url="http://www.ipea.gov.br/geobr/metadata/metadata.rds", httr::write_disk(tempf, overwrite = T))
     metadata <- readr::read_rds(tempf)
   }
-  
-  
+
+
   # Select geo
   temp_ano <- subset(metadata, geo=="country")
-  
-  
+
+
   # Verify year input
-  if (is.null(year)){ cat("Using data from year 2010\n")
+  if (is.null(year)){ message("Using data from year 2010\n")
     temp_ano <- subset(temp_ano, year==2010)
-    
+
   } else if (year %in% temp_ano$year){ temp_ano <- temp_ano[temp_ano[,2] == year, ]
-  
+
   } else { stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ",
                        paste(unique(temp_ano$year),collapse = " ")))
   }
-  
-  
+
+
   # list paths of files to download
   filesD <- as.character(temp_ano$download_path)
-  
+
   # download files
   temps <- paste0(tempdir(),"/", unlist(lapply(strsplit(filesD,"/"),tail,n=1L)))
   httr::GET(url=filesD, httr::progress(), httr::write_disk(temps, overwrite = T))
-  
+
   # read sf
   temp_sf <- readr::read_rds(temps)
-  
+
   return(temp_sf)
 }
