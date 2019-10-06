9b63e83cfec1a33c5cfdba2f257134aec9f45f3f
rafapereirabr <rafa.pereira.br@gmail.com>
2019-10-06 12:37:34 -0300

    test covr

--- tests/testthat/test-download_fun.R
+++ tests/testthat/test-download_fun.R
@@ -10,13 +10,16 @@ test_that("download_fun", {
     #
     # # test
     # expect_equal(ncol(temp_sf), 3)
+
+
+
+  # arroz
+  # chopsuey de car de com legumne SEM OIMENTAO
+  # frango com gengibre
+  # pastelzinho com 6
+
 })
 
 
-
-# arroz
-# chopsuey de car de com legumne SEM OIMENTAO
-# frango com gengibre
-# pastelzinho com 6
 
 
--- tests/testthat/test-grid_state_correspondence_table.R
+++ tests/testthat/test-grid_state_correspondence_table.R
@@ -4,8 +4,10 @@ context("Data")
 test_that("grid_state_correspondence_table", {
 
   # load data
-  load( system.file("data/grid_state_correspondence_table.RData", package="geobr") )
+#  load( system.file("data/grid_state_correspondence_table.RData", package="geobr") )
 
+  data(grid_state_correspondence_table)
+  head(grid_state_correspondence_table)
   # test
   expect_equal(ncol(grid_state_correspondence_table), 3)
   expect_equal(nrow(grid_state_correspondence_table), 139)
