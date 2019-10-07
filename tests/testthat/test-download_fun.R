947b661f42b50f0ef3bcfd15242b3d968dbab694
rafapereirabr <rafa.pereira.br@gmail.com>
2019-10-06 22:49:02 -0300

    new fun download_metadata

--- tests/testthat/test-download_fun.R
+++ tests/testthat/test-download_fun.R
@@ -2,21 +2,7 @@ context("Download")
 
 test_that("download_fun", {
 
-
-  expect_that( geobr::download_fun(0), equals(4101) )
-
-    # # download
-    # temp_sf <- geobr::download_fun()
-    #
-    # # test
-    # expect_equal(ncol(temp_sf), 3)
-
-
-
-  # arroz
-  # chopsuey de car de com legumne SEM OIMENTAO
-  # frango com gengibre
-  # pastelzinho com 6
+  expect_that( download_fun(0), equals(4101) )
 
 })
 
