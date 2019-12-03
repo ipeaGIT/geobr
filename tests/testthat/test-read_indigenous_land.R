8ae8aecc71391d194de86b75d5f34f32666cb913
rafapereirabr <rafa.pereira.br@gmail.com>
2019-12-03 15:29:44 -0300

    minor fixes to tests

--- tests/testthat/test-read_indigenous_land.R
+++ tests/testthat/test-read_indigenous_land.R
@@ -19,7 +19,7 @@ test_that("read_indigenous_land", {
   expect_equal(test_sf$code_terrai %>% length(), 615)
 
   # check projection
-#  expect_equal(sf::st_crs(test_sf)[[2]], "+proj=longlat +ellps=GRS80 +no_defs")
+  expect_equal(sf::st_crs(test_sf)[[2]], "+proj=longlat +ellps=GRS80 +towgs84=0.0,0.0,0.0,0.0,0.0,0.0,0.0 +no_defs")
 
 })
 
