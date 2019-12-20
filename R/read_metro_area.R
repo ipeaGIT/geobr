ad8bd264ac2876f65d0456cd69f929ef3701a600
rafapereirabr <rafa.pereira.br@gmail.com>
2019-12-20 16:58:00 -0300

    new function metropolitan area

--- R/read_metro_area.R
+++ R/read_metro_area.R
@@ -1,19 +1,9 @@
-#> DATASET: metropolitan areas 2000 - 2018
-#> Source: IBGE - "ftp://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/municipios_por_regioes_metropolitanas/"
-#: scale ________
-#> Metadata:
-# Titulo: Regioes Metropolitanas
-# Frequencia de atualizacao: Anual
-#
-# Forma de apresentação: Shape
-# Linguagem: Pt-BR
-# Character set: Utf-8
-#
-# Resumo: Poligonos de municipios de regioes metropolitanas do Brasil
-# Informações adicionais: Regioes metropolitanas definidas por legislacao estadual
-#
-# Informacao do Sistema de Referencia: SIRGAS 2000
-#
+#' Download shape files of official metropolitan areas in Brazil as an sf object.
+#'
+#' Metropolitan areas are created by each state in Brazil. The data set includes the municipalities that belong to
+#' all metropolitan areas in the country according to state legislation in each year. Orignal data were generated
+#' by Institute of Geography. Data at scale 1:250,000, using Geodetic reference system "SIRGAS2000" and CRS(4674).
+#'
 #'
 #' @param year A year number in YYYY format
 #' @export
@@ -22,15 +12,15 @@
 #' library(geobr)
 #'
 #' # Read all official metropolitan areas for a given year
-#   d <- read_metro_area(2005)
-# }
-#
-#
-#
-#
-# d <- read_metro_area(2005)
-
+#'   m <- read_metro_area(2005)
+#'
+#'   m <- read_metro_area(2018)
+#' }
+#'
+#'
+#'
 read_metro_area <- function(year){
+
 
   # Get metadata with data addresses
   metadata <- download_metadata()
@@ -38,7 +28,11 @@ read_metro_area <- function(year){
   # Select geo
   temp_meta <- subset(metadata, geo=="metropolitan_area")
 
-  # Verify year input
+  # 1.1 Verify year input
+  if (is.null(year)){  stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ",
+                                   paste(unique(temp_meta$year),collapse = " "))) }
+
+  # 1.2 Verify year input
   if (year %in% temp_meta$year){ message(paste0("Using year ",year))
     temp_meta <- temp_meta[temp_meta[,2] == year,]
   } else { stop(paste0("Error: Invalid Value to argument 'year'. It must be one of the following: ",
@@ -56,9 +50,3 @@ read_metro_area <- function(year){
   temp_sf <- readr::read_rds(temps)
   return(temp_sf)
 }
-
-
-
-# a05 <- read_metro_area(year = 2018)
-# plot(a05)
-#
