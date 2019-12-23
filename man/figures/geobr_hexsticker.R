d03026d8c41bde87c3afac1ca40d9ec3edf85c7f
rafapereirabr <rafa.pereira.br@gmail.com>
2019-12-23 20:45:22 -0300

    update geobr logo and add .svg file

--- man/figures/geobr_hexsticker.R
+++ man/figures/geobr_hexsticker.R
@@ -3,9 +3,16 @@ library(ggplot2)
 library(sf)
 library(geobr)
 
+
+# add special text font
+library(sysfonts)
+font_add_google(name = "Roboto", family = "Roboto")
+# font_add_google(name = "HelveticaR", family = "Linotype")
+
 library(extrafont)
 font_import()
 loadfonts(device = "win")
+
 
 
 # Download shape files
@@ -26,34 +33,52 @@ loadfonts(device = "win")
   as.numeric(object.size(uf_s))/  as.numeric(  object.size(uf))
 
 
-# add special text font
-  library(sysfonts)
-  font_add_google(name = "Roboto", family = "Roboto")
-  # font_add_google(name = "HelveticaR", family = "Linotype")
 
 
 
 ### Yellow and blue logo ---------------
 
+# .png
 plot_y <-
   ggplot() +
-  geom_sf(data = meso, fill=NA, size=.15, color="#272D67") +
-  geom_sf(data = uf, fill=NA, size=.4, color="#2E3946") +
+  geom_sf(data = meso_s, fill=NA, size=.15, color="#272D67") +
+  geom_sf(data = uf_s, fill=NA, size=.4, color="#2E3946") +
   theme_void() +
   theme(panel.grid.major=element_line(colour="transparent")) +
   #  theme(legend.position = "none") +
   annotate("text", x = -67.7, y = -20, label= "geobr", color="black",
-           size = 5, family = "Roboto", fontface="bold", angle = 0) # (.png  size = 25)(.svg  size = 6)
+           size = 25, family = "Roboto", fontface="bold", angle = 0) # (.png  size = 25)(.svg  size = 6)
 
 
-# .png
+
 sticker(plot_y, package="",
         s_x=1.12, s_y=.9, s_width=1.8, s_height=1.8, # ggplot image size and position
         h_fill="#FEB845", h_color="#FE9F45", # hexagon
-        filename="./man/figures/geobr_logo_y2.png", dpi=400)  # output name and resolution
+        filename="./man/figures/geobr_logo_y.png", dpi=400)  # output name and resolution
 
 
 beepr::beep()
+
+
+
+# .svg
+plot_y_svg <-
+  ggplot() +
+  geom_sf(data = meso_s, fill=NA, size=.08, color="#272D67") +
+  geom_sf(data = uf_s, fill=NA, size=.35, color="#2E3946") +
+  theme_void() +
+  theme(panel.grid.major=element_line(colour="transparent")) +
+  #  theme(legend.position = "none") +
+  annotate("text", x = -67.7, y = -20, label= "geobr", color="black",
+           size = 6, family = "Roboto", fontface="bold", angle = 0) # (.png  size = 25)(.svg  size = 6)
+
+
+
+sticker(plot_y_svg, package="",
+        s_x=1.12, s_y=.9, s_width=1.8, s_height=1.8, # ggplot image size and position
+        h_fill="#FEB845", h_color="#FE9F45", # hexagon
+        filename="./man/figures/geobr_logo_y.svg")  # output name and resolution
+
 
 
 
@@ -63,13 +88,13 @@ beepr::beep()
 # .png
 plot_b <-
   ggplot() +
-  geom_sf(data = meso, fill=NA, size=.15, color="#FEB845") +
-  geom_sf(data = uf, fill=NA, size=.4, color="#FEBF57") +
+  geom_sf(data = meso_s, fill=NA, size=.15, color="#FEB845") +
+  geom_sf(data = uf_s, fill=NA, size=.4, color="#FEBF57") +
   theme_void() +
   theme(panel.grid.major=element_line(colour="transparent")) +
   #  theme(legend.position = "none") +
   annotate("text", x = -67.7, y = -20, label= "geobr", color="#FEB845",
-           size = 6, family = "Roboto", fontface="bold", angle = 0) # (.png  size = 25)(.svg  size = 6)
+           size = 25, family = "Roboto", fontface="bold", angle = 0) # (.png  size = 25)(.svg  size = 6)
 
 
 sticker(plot_b, package="",
@@ -81,20 +106,20 @@ sticker(plot_b, package="",
 
 # .svg
 
-plot_b <-
+plot_b_svg <-
   ggplot() +
-#  geom_sf(data = meso, fill=NA, size=.15, color="#FEB845") +
-  geom_sf(data = uf, fill=NA, size=.4, color="#FEBF57") +
+  geom_sf(data = meso_s, fill=NA, size=.08, color="#FEB845") +
+  geom_sf(data = uf_s, fill=NA, size=.35, color="#FEBF57") +
   theme_void() +
   theme(panel.grid.major=element_line(colour="transparent")) +
   #  theme(legend.position = "none") +
   annotate("text", x = -67.7, y = -20, label= "geobr", color="#FEB845",
            size = 6, family = "Roboto", fontface="bold", angle = 0) # (.png  size = 25)(.svg  size = 6)
 
-sticker(plot_b, package="",
+sticker(plot_b_svg, package="",
         s_x=1.12, s_y=.9, s_width=1.8, s_height=1.8, # ggplot image size and position
         h_fill="#2D3E50", h_color="#1f2b38", h_size=1.5, # hexagon
-        filename="./man/figures/geobr_logo_b3.svg")  # output name and resolution
+        filename="./man/figures/geobr_logo_b.svg")  # output name and resolution
 
 
 beepr::beep()
