1b1cc702ba24e8d9f7490973e1285c7009f689d3
rafapereirabr <rafa.pereira.br@gmail.com>
2020-02-13 21:03:55 -0300

    read_amazon to geopackage

--- r-package/R/read_amazon.R
+++ r-package/R/read_amazon.R
@@ -4,6 +4,7 @@
 #' data comes from the Brazilian Ministry of Environment (MMA) and can be found at http://mapas.mma.gov.br/i3geo/datadownload.htm .
 #'
 #' @param year A date number in YYYY format (defaults to 2012)
+#' @param mode Whether the function returns the 'original' dataset with high resolution or a dataset with 'simplified' borders (Default)
 #' @export
 #' @family general area functions
 #' @examples \donttest{
@@ -14,7 +15,7 @@
 #'   a <- read_amazon(year=2012)
 #'}
 #'
-read_amazon <- function(year=NULL){
+read_amazon <- function(year=NULL, mode="simplified"){
 
   # Get metadata with data addresses
   metadata <- download_metadata()
@@ -22,6 +23,13 @@ read_amazon <- function(year=NULL){
 
   # Select geo
   temp_meta <- subset(metadata, geo=="amazonia_legal")
+
+  # Select mode
+  if(mode=="original"){
+    temp_meta <- temp_meta[  grepl(pattern="original", temp_meta$download_path), ]
+    } else {
+    temp_meta <- temp_meta[  grepl(pattern="simplified", temp_meta$download_path), ]
+  }
 
 
   # 1.1 Verify year input
@@ -47,6 +55,6 @@ read_amazon <- function(year=NULL){
   httr::GET(url=filesD, httr::progress(), httr::write_disk(temps, overwrite = T))
 
   # read sf
-  temp_sf <- readr::read_rds(temps)
+  temp_sf <- sf::st_read(temps)
   return(temp_sf)
 }
