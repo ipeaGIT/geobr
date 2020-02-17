532668afd613a92b4667b544b1ec732ccf757672
joaoc <joao.carabetta@gmail.com>
2020-02-17 17:45:31 -0500

    Write documentation and test for get_metadata and download_gpkg

--- python-package/geobr/utils.py
+++ python-package/geobr/utils.py
@@ -4,6 +4,10 @@ import tempfile
 import geopandas as gpd
 from functools import lru_cache
 
+def _get_unique_values(_df, column):
+
+    return ', '.join([str(i) for i in _df['{column}'].unique()])
+
 @lru_cache(maxsize=124)
 def download_metadata(
     url='http://www.ipea.gov.br/geobr/metadata/metadata_gpkg.csv'):
@@ -73,12 +77,13 @@ def apply_year(metadata, year):
         years = ', '.join([str(i) for i in metadata['year'].unique()])
 
         raise Exception('Error: Invalid Value to argument year. '
-                        'It must be one of the following: ' + years 
+                        'It must be one of the following: '
+                        f'{_get_unique_values[metadata, "year"]}'
                         )
     
     return metadata.query(f'year == {year}')
 
-def apply_tp(metadata, tp):
+def apply_data_type(metadata, data_type):
     """Filter metadata by data type. It can be simplified or normal. 
     The 'simplified' returns a simplified version of the shapefiles.
     'normal' returns the complete version. Usually, the complete version
@@ -104,10 +109,10 @@ def apply_tp(metadata, tp):
         If 'tp' is not found.
     """
 
-    if tp == "simplified":    
+    if data_type == "simplified":    
         return metadata[metadata['download_path'].str.contains("simplified")]
     
-    elif tp == "normal":
+    elif data_type == "normal":
         return metadata[~metadata['download_path'].str.contains("simplified")]
     
     else:
@@ -115,10 +120,32 @@ def apply_tp(metadata, tp):
                         It must be 'simplified' or 'normal'")
 
 
-@lru_cache(maxsize=124)
+@lru_cache(maxsize=1240)
 def load_gpkg(url):
+    """Internal function to donwload and convert to geopandas one url.
+
+    It caches url result for the active session.
     
-    content = requests.get(url).content
+    Parameters
+    ----------
+    url : str
+        Address with gpkg
+    
+    Returns
+    -------
+    gpd.GeoDataFrame
+         Table with metadata and shapefiles contained in url.
+    """
+    
+    try:
+        content = requests.get(url).content
+
+    except Exception as e:
+
+        raise Exception(
+            'Some interal url is broken.'
+            'Please report to https://github.com/ipeaGIT/geobr/issues'
+        )
     
     with tempfile.NamedTemporaryFile(suffix='.gpkg') as fp:
 
@@ -129,8 +156,19 @@ def load_gpkg(url):
     return gdf
 
 def download_gpkg(metadata):
+    """Generalizes gpkg dowload and conversion to geopandas
+    for one or many urls
     
-    # TODO: write docstring and tests
+    Parameters
+    ----------
+    metadata : pd.DataFrame
+        Filtered metadata
+    
+    Returns
+    -------
+    gpd.GeoDataFrame
+        Table with metadata and shapefiles contained in urls.
+    """
     
     urls = metadata['download_path'].tolist()
 
@@ -138,16 +176,44 @@ def download_gpkg(metadata):
     
     return gpd.GeoDataFrame(pd.concat(gpkgs, ignore_index=True))
 
-def get_metadata(geo, tp="simplified", year=None, year_default=None):
+def get_metadata(geo, data_type, year):
+    """Downloads and filters metadata given `geo`, `data_type` and `year`.
+    
+    Parameters
+    ----------
+    geo : str
+        Shapefile category. I.e: state, biome, etc...
+    data_type : str
+        `simplified` or `normal` shapefiles
+    year : int
+        Year of the data
+    
+    Returns
+    -------
+    pd.DataFrame
+        Filtered metadata
+    
+    Raises
+    ------
+    Exception
+        if a parameter is not found in metadata table
+    """
 
     # Get metadata with data addresses
     metadata = download_metadata()
 
+    if len(metadata.query(f'geo == "{geo}"')) == 0:
+        raise Exception(
+            f'The `geo` argument {geo} does not exist.'
+            'Please, use one of the following:'
+            f'{_get_unique_values[metadata, "geo"]}'
+    )
+
     # Select geo
     metadata = metadata.query(f'geo == "{geo}"')
 
-    # Select mode
-    metadata = apply_tp(metadata, tp)
+    # Select data type
+    metadata = apply_data_type(metadata, data_type)
     
     # Verify year input
     metadata = apply_year(metadata, year)
--- python-package/tests/test_utils.py
+++ python-package/tests/test_utils.py
@@ -1,8 +1,12 @@
 import pytest
-import geobr
 from time import time
-from geobr.utils import apply_year, apply_tp
 import pandas as pd
+import geopandas as gpd
+
+import geobr
+from geobr.utils import apply_year, apply_data_type, download_gpkg, load_gpkg,\
+    get_metadata
+
 
 @pytest.fixture
 def metadata_file():
@@ -42,18 +46,68 @@ def test_apply_year():
        assert apply_year(metadata, 'as')
        assert apply_year(metadata, 2324.12)
 
-def test_tp():
+def test_data_type():
 
     metadata = pd.DataFrame(['url_simplified', 'url'], 
                             columns=["download_path"]) 
     
-    assert apply_tp(metadata, 'simplified')['download_path'].unique()[0] \
+    assert apply_data_type(metadata, 'simplified')['download_path'].unique()[0] \
             == 'url_simplified'
     
-    assert apply_tp(metadata, 'normal')['download_path'].unique()[0] \
+    assert apply_data_type(metadata, 'normal')['download_path'].unique()[0] \
             == 'url'
     
     with pytest.raises(Exception):
-       assert apply_tp(metadata, 'slified')
-       assert apply_tp(metadata, None)
-       assert apply_tp(metadata, 2324.12)
+       assert apply_data_type(metadata, 'slified')
+       assert apply_data_type(metadata, None)
+       assert apply_data_type(metadata, 2324.12)
+
+def test_load_gpkg():
+
+    valid_url = 'http://www.ipea.gov.br/geobr/data_gpkg/amazonia_legal/2012/amazonia_legal.gpkg'
+
+    assert isinstance(load_gpkg(valid_url), gpd.geodataframe.GeoDataFrame)
+    
+    # Test Cache
+    start_time = time()
+    load_gpkg(valid_url)
+    assert time() - start_time < 1
+    
+
+    with pytest.raises(Exception):
+        isinstance(load_gpkg('asd'), gpd.geodataframe.GeoDataFrame)
+        isinstance(load_gpkg(1234), gpd.geodataframe.GeoDataFrame)
+        isinstance(load_gpkg(valid_url + 'asdf'), 
+                    gpd.geodataframe.GeoDataFrame)
+
+def test_download_gpkg():
+
+    multiple_metadata = pd.DataFrame([
+        'http://www.ipea.gov.br/geobr/data_gpkg/amazonia_legal/2012/amazonia_legal.gpkg',
+        'http://www.ipea.gov.br/geobr/data_gpkg/meso_regiao/2014/17ME_simplified.gpkg'], 
+                            columns=["download_path"]) 
+
+    single_metadata = pd.DataFrame([
+        'http://www.ipea.gov.br/geobr/data_gpkg/amazonia_legal/2012/amazonia_legal.gpkg'],
+                            columns=["download_path"]) 
+ 
+    # assert isinstance(download_gpkg(multiple_metadata), 
+    #                   gpd.geodataframe.GeoDataFrame)
+    assert isinstance(download_gpkg(single_metadata), 
+                      gpd.geodataframe.GeoDataFrame)
+
+def test_get_metadata():
+
+    assert isinstance(get_metadata('state', 'simplified', 2010), 
+                    pd.core.frame.DataFrame)
+
+    assert isinstance(get_metadata('state', 'simplified', None), 
+                    pd.core.frame.DataFrame)
+
+    assert isinstance(get_metadata('state', 'normal', None), 
+                    pd.core.frame.DataFrame)
+
+    with pytest.raises(Exception):
+        get_metadata(123, 123, 123)
+        get_metadata('state', 123, 123)
+        get_metadata('state', 'simplified', 12334)
