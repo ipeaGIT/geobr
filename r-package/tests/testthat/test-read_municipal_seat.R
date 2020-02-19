c69a52f4d35a0829dee78da9ff50ce9ca5311add
rafapereirabr <rafa.pereira.br@gmail.com>
2020-02-19 19:07:00 -0300

    skip test of heaviest functions on travis

--- r-package/tests/testthat/test-read_municipal_seat.R
+++ r-package/tests/testthat/test-read_municipal_seat.R
@@ -1,9 +1,9 @@
 context("read_municipal_seat")
 
 # skip tests because they take too much time
-testthat::skip_on_cran()
-# testthat::skip_on_travis()
 # skip_if(Sys.getenv("TEST_ONE") != "")
+testthat::skip_on_cran()
+#testthat::skip_on_travis()
 
 # Reading the data -----------------------
 
