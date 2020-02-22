0c3945258801311cbc21e7920cb1637364a80db9
Rafael H M Pereira <rafa.pereira.br@gmail.com>
2020-02-21 21:24:34 -0300

    Test muni on Travis

--- r-package/tests/testthat/test-read_municipality.R
+++ r-package/tests/testthat/test-read_municipality.R
@@ -2,7 +2,7 @@ context("read_municipality")
 
 # skip tests because they take too much time
 testthat::skip_on_cran()
- testthat::skip_on_travis()
+# testthat::skip_on_travis()
 # skip_if(Sys.getenv("TEST_ONE") != "")
 
 
