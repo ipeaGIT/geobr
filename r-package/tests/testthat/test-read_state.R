df75c4788e34133d28d00f68d15442b3ebf1f7ab
rafapereirabr <rafa.pereira.br@gmail.com>
2020-02-22 15:50:20 -0300

    test-read_state.R 100%

--- r-package/tests/testthat/test-read_state.R
+++ r-package/tests/testthat/test-read_state.R
@@ -9,35 +9,27 @@ testthat::skip_on_cran()
 test_that("read_state", {
 
   # read data
-  test_state_abrev <- read_state(code_state="AC", year=2010)
-  test_state_abrev2 <- read_state(code_state="AP")
-  test_state_abrev3 <- read_state(code_state='all', year=1872)
+  testthat::expect_output( read_state() )
+  testthat::expect_output( read_state(code_state=11, year=1991) )
 
-  test_state_code <- read_state(code_state=11, year=2010)
-  test_state_code2 <- read_state(code_state=11)
-#  test_state_code3 <- read_state(code_state=11, year=1872)
+  testthat::expect_output( read_state(code_state="AC", year=2010) )
+  testthat::expect_output( read_state(code_state=11, year=2010) )
+  testthat::expect_output( read_state(code_state="all") )
 
-  test_all <- read_state(code_state="all", year=2010)
-  test_all2 <- read_state(code_state="all")
-  test_all3 <- read_state(code_state="all", year=1872)
+
+
+
+  test_code <- read_state(code_state=11, year=2010)
 
   # check sf object
-  expect_true(is(test_state_abrev, "sf"))
-  expect_true(is(test_state_abrev2, "sf"))
-  expect_true(is(test_state_abrev3, "sf"))
-  expect_true(is(test_state_code, "sf"))
-  expect_true(is(test_state_code2, "sf"))
-  # expect_true(is(test_state_code3, "sf"))
-  expect_true(is(test_all, "sf"))
-  expect_true(is(test_all2, "sf"))
-  expect_true(is(test_all3, "sf"))
+  expect_true(is(test_code, "sf"))
 
-  # check number of micro
-  expect_equal(test_all$code_state %>% length(), 27)
-  expect_equal(test_all3$name_state %>% length(), 21)
+
+  # check number of weighting areas
+  expect_equal(nrow(test_code), 1)
 
   # check projection
-  expect_equal(sf::st_crs(test_all)[[2]], "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
+  expect_equal(sf::st_crs(test_code)[[2]], "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
 
 })
 
@@ -49,34 +41,15 @@ test_that("read_state", {
 
   # Wrong year and code
   expect_error(read_state(code_state=9999999, year=9999999))
-  expect_error(read_state(code_state=9999999, year="xxx"))
-  expect_error(read_state(code_state="xxx", year=9999999))
-  expect_error(read_state(code_state="xxx", year="xxx"))
-  expect_error(read_state(code_state=NULL, year=9999999))
-  expect_error(read_state(code_state=NULL, year="xxx"))
-
-  # Wrong year
-  expect_error(read_state(code_state=11, year=9999999))
-  expect_error(read_state(code_state=1401, year=9999999))
-  expect_error(read_state(code_state=11008, year=9999999))
-  expect_error(read_state(code_state=11, year= "xx"))
-  expect_error(read_state(code_state=1401, year= "xx"))
-  expect_error(read_state(code_state=11008, year= "xx"))
-
-  expect_error(read_state(code_state="all", year=9999999))
-  expect_error(read_state(code_state="SC", year=9999999))
-  expect_error(read_state(code_state="SC", year="xx"))
-  expect_error(read_state(code_state="all", year="xx"))
 
   # Wrong code
-#  expect_message(read_state(code_state=9999999, year=1991))
-  expect_error(read_state(code_state=9999999, year=2000))
+  testthat::expect_error( read_state(code_state=NULL, year=1991) ) # EXception
+
   expect_error(read_state(code_state=9999999))
-#  expect_message(read_state(code_state="XXX", year=1991))
-  expect_error(read_state(code_state="XXX", year=2000))
-  expect_error(read_state(code_state="XXX"))
-#  expect_message(read_state(code_state=NULL, year=1991))
-  expect_error(read_state(code_state=NULL, year=2000))
-  expect_error(read_state(code_state=NULL))
+  expect_error(read_state(code_state=5201108312313213123123123))
+  expect_error(read_state(code_state="AC_ABCD"))
+
+  # Wrong year
+  expect_error(read_state( year=9999999))
 
 })
