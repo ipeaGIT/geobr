198831b5ae9bca20e18ea963669006b9fcdbef46
rafapereirabr <rafa.pereira.br@gmail.com>
2020-02-23 09:52:20 -0300

    simplify test_read_state. this way gives 100% covr in R

--- python-package/tests/test_read_state.py
+++ python-package/tests/test_read_state.py
@@ -4,41 +4,32 @@ from geobr import read_state
 
 def test_read_state():
 
+    assert isinstance(read_state(), 
+                      gpd.geodataframe.GeoDataFrame)
+
+    assert isinstance(read_state(code_state=11, year=1991), 
+                      gpd.geodataframe.GeoDataFrame)
+
     assert isinstance(read_state(code_state="AC", year=2010), 
-                      gpd.geodataframe.GeoDataFrame)
-
-    assert isinstance(read_state(code_state="AP"), 
-                      gpd.geodataframe.GeoDataFrame)
-
-    assert isinstance(read_state(code_state="all", year=1872), 
                       gpd.geodataframe.GeoDataFrame)
 
     assert isinstance(read_state(code_state=11, year=2010), 
                       gpd.geodataframe.GeoDataFrame)
 
-    assert isinstance(read_state(code_state=11), 
+    assert isinstance(read_state(code_state="all"), 
                       gpd.geodataframe.GeoDataFrame)
 
     with pytest.raises(Exception):
         read_state(code_state=9999999, year=9999999)
-        read_state(code_state=9999999, year="xxx")
-        read_state(code_state="xxx", year=9999999)
-        read_state(code_state="xxx", year="xxx")
-        read_state(code_state=None, year=9999999)
-        read_state(year="xxx")
-
-        read_state(code_state=11, year=9999999)
-        read_state(code_state=1401, year=9999999)
-        read_state(code_state=11008, year=9999999)
-        read_state(code_state=11, year= "xx")
-        read_state(code_state=1401, year= "xx")
-        read_state(code_state=11008, year= "xx")
-        read_state(code_state="all", year=9999999)
-        read_state(code_state="SC", year=9999999)
-        read_state(code_state="SC", year="xx")
-        read_state(code_state="all", year="xx")
-        read_state(code_state=9999999, year=2000)
+        
+        # Wrong year and code
+        read_state(code_state=9999999, year=9999999))
+        
+        # Wrong code
+        expect_error( read_state(code_state=NULL, year=1991)
         read_state(code_state=9999999)
-        read_state(code_state="XXX", year=2000)
-        read_state(code_state="XXX")
-        read_state()
+        read_state(code_state=5201108312313213123123123)
+        read_state(code_state="AC_ABCD")
+        
+        # Wrong year
+        read_state( year=9999999)
