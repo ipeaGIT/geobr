6b97e1ce92cbc48f93bc85eee9d8537b8fd47540
joaoc <joao.carabetta@gmail.com>
2020-03-04 23:12:14 -0500

    translate immediate region

---
+++ python-package/tests/test_read_immediate_region.py
@@ -0,0 +1,20 @@
+import geopandas as gpd
+import pytest
+from geobr import read_immediate_region
+
+def test_read_immediate_region():
+
+    assert isinstance(read_immediate_region(), 
+                      gpd.geodataframe.GeoDataFrame)
+
+    assert isinstance(read_immediate_region(code_immediate=11), 
+                      gpd.geodataframe.GeoDataFrame)
+
+    assert isinstance(read_immediate_region(code_immediate='AC'), 
+                      gpd.geodataframe.GeoDataFrame)
+
+    assert len(read_immediate_region(code_immediate=110002)) == 8
+
+    with pytest.raises(Exception):
+        read_immediate_region(year=9999999)
+        read_immediate_region(code_intermediate=5201108312313213)
