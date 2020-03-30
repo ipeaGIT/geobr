0e60a2771b58437e754e80b93c8931626988206f
rafapereirabr <rafa.pereira.br@gmail.com>
2020-03-30 18:26:19 -0300

    new support function for prep_ to dissolve borders

--- r-package/prep_data/prep_functions.R
+++ r-package/prep_data/prep_functions.R
@@ -102,3 +102,71 @@ simplify_temp_sf <- function(temp_sf, tolerance=100){
   temp_gpkg_simplified <- lwgeom::st_make_valid(temp_gpkg_simplified)
   return(temp_gpkg_simplified)
 }
+
+
+
+###### Dissolve borders temp_sf -----------------
+
+## Function to clean and dissolve the borders of polygons by groups
+dissolve_polygons <- function(mysf, group_column){
+
+
+  # a) make sure we have valid geometries
+  temp_sf <- lwgeom::st_make_valid(mysf)
+  temp_sf <- temp_sf %>% st_buffer(0)
+
+  # b) make sure we have sf MULTIPOLYGON
+  temp_sf1 <- temp_sf %>% st_cast("MULTIPOLYGON")
+
+  # c) long but complete dissolve function
+  dissolvefun <- function(grp){
+
+    # c.1) subset region
+    temp_region <- subset(mysf, get(group_column, mysf)== grp )
+
+
+    # c.2) create attribute with the number of points each polygon has
+    points_in_each_polygon = sapply(1:dim(temp_region)[1], function(i)
+      length(st_coordinates(temp_region$geom[i])))
+
+    temp_region$points_in_each_polygon <- points_in_each_polygon
+    mypols <- subset(temp_region, points_in_each_polygon > 0)
+
+    # d) convert to sp
+    sf_regiona <- mypols %>% as("Spatial")
+    sf_regiona <- rgeos::gBuffer(sf_regiona, byid=TRUE, width=0) # correct eventual topology issues
+
+    # c) dissolve borders to create country file
+    result <- maptools::unionSpatialPolygons(sf_regiona, rep(TRUE, nrow(sf_regiona@data))) # dissolve
+
+
+    # d) get rid of holes
+    outerRings = Filter(function(f){f@ringDir==1},result@polygons[[1]]@Polygons)
+    outerBounds = sp::SpatialPolygons(list(sp::Polygons(outerRings,ID=1)))
+
+    # e) convert back to sf data
+    outerBounds <- st_as_sf(outerBounds)
+    outerBounds <- st_set_crs(outerBounds, st_crs(mysf))
+    st_crs(outerBounds) <- 4674
+
+    # retrieve code_region info and reorder columns
+    outerBounds <- dplyr::mutate(outerBounds, group_column = grp)
+    outerBounds <- dplyr::select(outerBounds, group_column, geometry)
+    return(outerBounds)
+  }
+
+
+  # Apply sub-function
+  groups_sf <- pbapply::pblapply(X = unique(get(group_column, mysf)), FUN = dissolvefun )
+
+  # rbind results
+  temp_sf <- do.call('rbind', groups_sf)
+  return(temp_sf)
+}
+
+
+# # test
+# states <- geobr::read_state()
+# a <- dissolve_polygons(states, group_column='code_region')
+# plot(a)
+
