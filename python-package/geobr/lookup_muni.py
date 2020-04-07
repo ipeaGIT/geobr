0fede603b558c93c21860f3234d756615a7493bf
joaoc <joaoc@iMac-de-Juan.local>
2020-04-07 15:29:22 -0300

    small changes to doc and style

--- python-package/geobr/lookup_muni.py
+++ python-package/geobr/lookup_muni.py
@@ -7,8 +7,12 @@ except ModuleNotFoundError:
 def lookup_muni(name_muni=None, code_muni=None, verbose=False):
     """ Lookup municipality codes and names.
 
-    Input a municipality NAME or CODE and get the names and codes of the municipality's corresponding state, meso, micro,
-    intermediate, and immediate regions. You should not select both code_muni and name_muni
+    By default, it looks for all municipalities. You can also use 'all' to in
+    `name_muni` or `code_muni` to get all municipalities. 
+
+    Input a municipality NAME or CODE and get the names and codes of 
+    the municipality's corresponding state, meso, micro, intermediate, and 
+    immediate regions. You should not select both code_muni and name_muni
 
     Parameters
     ----------
@@ -24,7 +28,8 @@ def lookup_muni(name_muni=None, code_muni=None, verbose=False):
 
     Returns
     -------
-    data.frame with 13 columns identifying the geographies information of that municipality
+    pd.DataFram
+    13 columns identifying the geographies information of that municipality
 
     Details Only available from 2010 Census data so far
 
@@ -42,44 +47,57 @@ def lookup_muni(name_muni=None, code_muni=None, verbose=False):
     >>> mun = lookup_muni(3304557)
 
     # lookup table for all municipalities
-    >>> mun_all = lookup_muni("all")
+    >>> mun_all = lookup_muni()
     """
     # Get metadata with data url addresses
     temp_meta = utils.select_metadata(geo='lookup_muni', year=2010)
 
     # Read DataFrame available at provided url
-    lookup_table_2010 = utils.download_metadata(temp_meta.loc[:, 'download_path'].to_list()[0])
-    lookup_table_2010['name_muni_format'] = lookup_table_2010['name_muni_format'].str.lower()
+    lookup_table = utils.download_metadata(
+                            temp_meta.loc[:, 'download_path'].to_list()[0])
+    lookup_table['name_muni_format'] = lookup_table['name_muni_format'].str.lower()
 
     # Search by inputs
-    if code_muni == 'all' or name_muni == 'all' or (code_muni is None and name_muni is None):
+    if (code_muni == 'all' or name_muni == 'all' or 
+       (code_muni is None and name_muni is None)):
         if verbose:
-            print(f"Returning results for all municipalities")
-        return lookup_table_2010.iloc[:, :-1]
+            print(f'Returning results for all municipalities')
+        return lookup_table.iloc[:, :-1]
+    
     elif code_muni is not None:
         if name_muni is not None:
             if verbose:
-                print("Ignoring argument name_muni")
+                print('Ignoring argument name_muni')
         try:
-            output = lookup_table_2010[lookup_table_2010['code_muni'] == int(code_muni)].iloc[:, :-1]
+            output = lookup_table[lookup_table['code_muni'] == \
+                     int(code_muni)].iloc[:, :-1]
             if verbose:
-                print(f"Returning results for municipality {output.loc[:, 'name_muni'].to_list()[0]}")
+                print('Returning results for municipality ',
+                      f'{output.loc[:, "name_muni"].to_list()[0]}')
             return output
+
         except KeyError:
-            raise Exception(f'The `code_muni` argument {code_muni} was not found in the database.')
+            raise Exception(f'The `code_muni` argument {code_muni}',
+                             'was not found in the database.')
+    
     elif name_muni is not None:
         # Cleaning from accents and turning into lower cases without spaces
         name_muni = utils.strip_accents(str(name_muni).lower().strip())
-        output = lookup_table_2010[lookup_table_2010['name_muni_format'] == name_muni]
+        output = lookup_table[
+                    lookup_table['name_muni_format'] == name_muni]
 
         if len(output) == 0:
             if verbose:
-                print("Please insert a valid municipality name")
-            raise Exception(f'The `name_muni` argument {name_muni} was not found in the database.')
+                print('Please insert a valid municipality name')
+            raise Exception(f'The `name_muni` argument {name_muni} ',
+                             'was not found in the database.')
         else:
             if verbose:
-                print(f"Returning results for municipality {output.loc[:, 'name_muni'].to_list()[0]}")
+                print('Returning results for municipality' 
+                      f'{output.loc[:, "name_muni"].to_list()[0]}')
             return output.iloc[:, :-1]
+
     elif code_muni == 'all' and name_muni == 'all':
         if verbose:
-            print("Please insert either a municipality name or a municipality code")
+            print('Please insert either a municipality ',
+                  'name or a municipality code')
--- python-package/tests/test_lookup_muni.py
+++ python-package/tests/test_lookup_muni.py
@@ -10,10 +10,13 @@ def test_lookup_muni():
     assert isinstance(lookup_muni.lookup_muni(code_muni=2304400), pd.DataFrame)
     assert isinstance(lookup_muni.lookup_muni(name_muni="all"), pd.DataFrame)
     assert isinstance(lookup_muni.lookup_muni(code_muni="all"), pd.DataFrame)
+    assert isinstance(lookup_muni.lookup_muni(), pd.DataFrame)
 
     # Check number of columns
     df = lookup_muni.lookup_muni(name_muni="rio de janeiro")
     assert len(df.columns) == 13
+
+    assert len(lookup_muni.lookup_muni()) > len(lookup_muni.lookup_muni(code_muni=2304400))
 
     # When using two arguments (supposed to give a warning)
     with pytest.raises(Exception):
