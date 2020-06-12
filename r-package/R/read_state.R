6564d9e421e2456865525037f19f593b50ee58f7
rafapereirabr <rafa.pereira.br@gmail.com>
2020-06-12 09:56:09 -0300

    fix error in read_state.R function

--- r-package/R/read_state.R
+++ r-package/R/read_state.R
@@ -48,29 +48,29 @@ if( x < 1992){
 #       stop("Error: Invalid Value to argument code_state.")
 #       }
 
-  if(is.null(code_state)){ stop("Value to argument 'code_state' cannot be NULL") }
-  
+  if(is.null(code_state)){ stop("Value to argument 'code_state' cannot be NULL")}
+
   if(code_state=="all"){ message("Loading data for the whole country\n")
-    
+
     # list paths of files to download
     file_url <- as.character(temp_meta$download_path)
-    
+
     # download gpkg
     temp_sf <- download_gpkg(file_url, progress_bar = showProgress)
     return(temp_sf)
-    
-  } if(nchar(code_state)==2){
-    
+
+  } else if(nchar(code_state)==2){
+
     # list paths of files to download
     file_url <- as.character(temp_meta$download_path)
-    
+
     # download gpkg
     temp_sf <- download_gpkg(file_url, progress_bar = showProgress)
-    
+
     temp_sf <- subset(temp_sf,code_state==substr(code_state, 1, 2))
     return(temp_sf)
   }
-  
+
 }  else {
 
 
