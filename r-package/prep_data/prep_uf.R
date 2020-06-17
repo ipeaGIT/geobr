364025c9f3c0b46553f8fe27d7ca5c1b38ba91e4
Pedrojorge7 <Pedrojorge_holanda@hotmail.com>
2020-06-17 18:01:16 -0300

    Ajustes

--- r-package/prep_data/prep_uf.R
+++ r-package/prep_data/prep_uf.R
@@ -7,9 +7,9 @@ source('./prep_data/malhas_municipais_function.R')
 
 ## function for malha municipal
 
-malhas_municipais(region='uf')
+#malhas_municipais(region='uf',year=2018)
 
-uf_dir <- "L:////# DIRUR #//ASMEQ//geobr//data-raw//malhas_municipais//shapes_in_sf_all_years_original/uf"
+uf_dir <- ".//shapes_in_sf_all_years_original/uf"
 sub_dirs <- list.dirs(path =uf_dir, recursive = F)
 
 
@@ -20,6 +20,17 @@ clean_states <- function( e ){ #  e <- sub_dirs[sub_dirs %like% 2000]
   # get year of the folder
   last4 <- function(x){substr(x, nchar(x)-3, nchar(x))}   # function to get the last 4 digits of a string
   year <- last4(e)
+  
+  # create directory to save original shape files in sf format
+  dir.create(file.path("shapes_in_sf_all_years_cleaned"), showWarnings = FALSE)
+  
+  # create a subdirectory of states, municipalities, micro and meso regions
+  dir.create(file.path("shapes_in_sf_all_years_cleaned/uf/"), showWarnings = FALSE)
+  
+  # create a subdirectory of years
+  dir.create(file.path(paste0("shapes_in_sf_all_years_cleaned/uf/",year)), showWarnings = FALSE)
+  gc(reset = T)
+  
   
   # list all sf files in that year/folder
   sf_files <- list.files(e, full.names = T)
@@ -34,28 +45,28 @@ clean_states <- function( e ){ #  e <- sub_dirs[sub_dirs %like% 2000]
       # dplyr::rename and subset columns
       names(temp_sf) <- names(temp_sf) %>% tolower()
       temp_sf <- dplyr::rename(temp_sf, code_state = geocodigo, name_state = nome)
-      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geometry'))
+      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geom'))
     }
     
     if (year %like% "2010"){
       # dplyr::rename and subset columns
       names(temp_sf) <- names(temp_sf) %>% tolower()
       temp_sf <- dplyr::rename(temp_sf, code_state = cd_geocodu, name_state = nm_estado)
-      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geometry'))
+      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geom'))
     }
     
     if (year %like% "2013|2014|2015|2016|2017|2018"){
       # dplyr::rename and subset columns
       names(temp_sf) <- names(temp_sf) %>% tolower()
       temp_sf <- dplyr::rename(temp_sf, code_state = cd_geocuf, name_state = nm_estado)
-      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geometry'))
+      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geom'))
     }
     
     if (year %like% "2019"){
       # dplyr::rename and subset columns
       names(temp_sf) <- names(temp_sf) %>% tolower()
       temp_sf <- dplyr::rename(temp_sf, code_state = cd_uf, name_state = nm_uf)
-      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geometry'))
+      temp_sf <- dplyr::select(temp_sf, c('code_state', 'name_state', 'geom'))
     }
     
     # add State abbreviation
@@ -67,7 +78,7 @@ clean_states <- function( e ){ #  e <- sub_dirs[sub_dirs %like% 2000]
     temp_sf <- add_region_info(temp_sf,'code_state')
     
     # reorder columns
-    temp_sf <- dplyr::select(temp_sf, 'code_state', 'abbrev_state', 'name_state', 'code_region', 'name_region', 'geometry')
+    temp_sf <- dplyr::select(temp_sf, 'code_state', 'abbrev_state', 'name_state', 'code_region', 'name_region', 'geom')
     
     # Use UTF-8 encoding
     temp_sf$name_state <- stringi::stri_encode(as.character((temp_sf$name_state), "UTF-8"))
@@ -81,7 +92,7 @@ clean_states <- function( e ){ #  e <- sub_dirs[sub_dirs %like% 2000]
     # Convert columns from factors to characters
     temp_sf %>% dplyr::mutate_if(is.factor, as.character) -> temp_sf
     
-    # Make any invalid geometry valid # st_is_valid( sf)
+    # Make any invalid geom valid # st_is_valid( sf)
     temp_sf <- sf::st_make_valid(temp_sf)
     
     # keep code as.numeric()
