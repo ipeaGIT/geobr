a7fe6333e3fb85d11a277b7944eb98b1f4a8c30f
rafapereirabr <rafa.pereira.br@gmail.com>
2020-07-07 16:32:16 -0300

    add new function read_health_region.R

---
+++ r-package/R/read_health_region.R
@@ -0,0 +1,37 @@
+#' Download official data of Brazilian health regions as an sf object.
+#'
+#' @param year Year of the data (defaults to 2013, latest available)
+#' @param simplified Logic FALSE or TRUE, indicating whether the function returns the
+#'  data set with 'original' resolution or a data set with 'simplified' borders (Defaults to TRUE).
+#'  For spatial analysis and statistics users should set simplified = FALSE. Borders have been
+#'  simplified by removing vertices of borders using st_simplify{sf} preserving topology with a dTolerance of 100.
+#' @param showProgress Logical. Defaults to (TRUE) display progress bar
+#' @param tp Argument deprecated. Please use argument 'simplified'
+#'
+#' @export
+#' @family general area functions
+#' @examples \donttest{
+#'
+#' library(geobr)
+#'
+#' # Read all health regions for a given year
+#'   hr <- read_health_region( year=2013)
+#'
+#'}
+#'
+read_health_region <- function(year=2013, simplified=TRUE, showProgress=TRUE, tp){
+
+  # deprecated 'tp' argument
+  if (!missing("tp")){stop(" 'tp' argument deprecated. Please use argument 'simplified' TRUE or FALSE")}
+
+  # Get metadata with data url addresses
+  temp_meta <- select_metadata(geography="health_region", year=year, simplified=simplified)
+
+  # list paths of files to download
+  file_url <- as.character(temp_meta$download_path)
+
+  # download files
+  temp_sf <- download_gpkg(file_url, progress_bar = showProgress)
+  return(temp_sf)
+
+}
