44fe140e454632c21b8198036e9d4427e6beca6d
Babisan08 <barbaracosta.est@gmail.com>
2020-07-24 17:44:18 -0300

    Update st_write

--- r-package/prep_data/prep_meso.R
+++ r-package/prep_data/prep_meso.R
@@ -93,11 +93,11 @@ clean_meso <- function(e){ #  e <- sub_dirs[2]
     
     i <- gsub(".rds", ".gpkg", i)
     
-    sf::st_write(temp_sf, i,layer_options = 'OVERWRITE=YES',update=TRUE)
+    sf::st_write(temp_sf, i, delete_layer=TRUE,update=TRUE)
     
     i <- gsub(".gpkg", "_simplified.gpkg", i)
     
-    sf::st_write(temp_sf_simplified, i, layer_options = 'OVERWRITE=YES', update=TRUE)
+    sf::st_write(temp_sf_simplified, i, delete_layer=TRUE, update=TRUE)
     
   }
 }
