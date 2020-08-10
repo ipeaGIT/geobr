ba0d8c6ca09190dcebeb1ebc47f9b86fe675a689
Pedro Cavalcante <pedrocolrj@gmail.com>
2020-08-10 20:43:27 -0300

    melhoras no download de dados brutos, instituição de geometria, agora preciso limpar

--- r-package/prep_data/prep_accidents.R
+++ r-package/prep_data/prep_accidents.R
@@ -30,117 +30,84 @@ source("./prep_data/prep_functions.R")
 # Directory to keep raw zipped files
 dir.create("./federal_roads_accidents")
 
-reference_year <- 2020
-reference_state <- NULL
+ref_year <- 2020
+ref_state <- NULL
 
 #### 1. Download and clean data, given a year and possibly a state -----------------
 
 get_raw_accidents <- function(reference_year,
-                              reference_state) {
+                              reference_state = "all") {
 
-html_content <-
-  read_html("https://portal.prf.gov.br/dados-abertos-acidentes") %>%
-  html_node("#acontent ul") %>%
-  html_nodes("li")
-
-tibble(
-  href = { html_content %>%
-   html_children %>%
-   html_attr("href") },
-  year = { html_content %>%
-    html_text() %>%
-    as.numeric() }) %>%
-  filter(year == reference_year,
-         if_else(is.null(reference_state), TRUE, uf == reference_state)) %>%
-  mutate(href = glue("curl {href}/download | funzip")) %>%
-  pull(href) %>%
-  fread(encoding = "Latin-1") %>%
-  as_tibble() %T>%
-  walk(function(.x) {
-
-    dir.create(glue("./federal_roads_accidents/{reference_year}"))
-    # Save original raw data
-    write_csv2(.x, glue("./federal_roads_accidents/{reference_year}/prf_raw_data"))
-
-  })
-
-}
-
-clean_raw_data <- function(raw_accidents) {
+  html_content <-
+    read_html("https://portal.prf.gov.br/dados-abertos-acidentes") %>%
+    html_node("#acontent ul") %>%
+    html_nodes("li")
 
 
 
+  data <- tibble(
+    href = {
+      html_content %>%
+        html_children %>%
+        html_attr("href")
+      },
+    year = {
+      html_content %>%
+        html_text() %>%
+        as.numeric() }
+    ) %>%
+    filter(year == reference_year) %>%
+    mutate(href = glue("curl {href}/download | funzip")) %>%
+    pull(href) %>%
+    fread(encoding = "Latin-1") %>%
+    as_tibble()
+
+  if(reference_state == "all") {
+    reference_state <- unique(data$uf)
+  }
+
+
+
+
+   data %<>%
+     filter(uf %in% reference_state) %>%
+     mutate(latitude = parse_double(latitude, locale = locale(decimal_mark = ',')),
+            longitude = parse_double(longitude, locale = locale(decimal_mark = ',')),
+            km = parse_double(km, locale = locale(decimal_mark = ',')),
+            id = as.character(id),
+            br = as.character(br))
+
+    data
 
 }
 
 #### 2. Create folders to save sf.rds files  -----------------
 
-# create directory to save cleaned shape files in sf format
-dir.create("./health_facilities/shapes_in_sf_all_years_cleaned", showWarnings = FALSE)
-
-
-
-
-
+accidents <- get_raw_accidents(year)
 
 #### 3. Save cleaned data sets in compact .rds format-----------------
 
-
-
-# Convert originl data frame into sf
-accidents_sf <- st_as_sf(x = accidents,
-                         coords = c("longitude", "latitude"),
-                         crs = "+proj=longlat +datum=WGS84")
-
-
-head(cnes_sf)
-table(cnes_sf$origem_dado)
-
-# create year_update column
-cnes_sf$year_update <- as.Date(cnes_sf$data_atualizacao) %>% format("%Y") %>% as.numeric()
-table(cnes_sf$year_update)
-
-# find most common year of update
-ux <- unique(cnes_sf$year_update)
-most_freq_year <- ux[which.max(tabulate(match(cnes_sf$year_update, ux)))]
+# Convert original data frame into sf
+accidents_sf <-
+  st_as_sf(accidents,
+           coords = c("longitude", "latitude"),
+           crs = "+proj=longlat +datum=WGS84")
 
 
 # Create dir to save data of that specific year
-dir.create(paste0("./health_facilities/shapes_in_sf_all_years_cleaned/",most_freq_year))
-
-
-
-
-
+dir.create(glue("./federal_roads_accidents/{reference_year}/prf_raw_data"),
+           showWarnings = FALSE)
+# Save original raw data
+write_csv2(data, glue("./federal_roads_accidents/{reference_year}/prf_raw_data/{reference_year}_{ifelse(length(reference_state) > 1, 'many', reference_state)}_raw.csv"))
 
 ### Change colnames
-head(cnes_sf)
-cnes_sf <- dplyr::select(cnes_sf,
-                         code_cnes = co_cnes,
-                         code_muni = co_ibge,
-                         code_state= code_state,
-                         abbrev_state= abbrev_state,
-                         date_update = data_atualizacao,
-                         year_update = year_update,
-                         data_source = origem_dado,
-                         geometry=geometry)
-
-head(cnes_sf)
-
-
 # Change CRS to SIRGAS  Geodetic reference system "SIRGAS2000" , CRS(4674).
-st_cres(cnes_sf)
-cnes_sf <- st_transform(cnes_sf, 4674)
-
-
-
-
-
+accidents_st <- st_transform(accidents_sf, 4674)
 
 
 
 # Save raw file in sf format
-write_rds(cnes_sf, paste0("./health_facilities/shapes_in_sf_all_years_cleaned/",most_freq_year,"/cnes_sf_",most_freq_year,".rds"), compress = "gz")
+write_rds(cnes_sf, paste0("./health_facilities/shapes_in_sf_all_years_cleaned/",year,"/cnes_sf_",most_freq_year,".rds"), compress = "gz")
 sf::st_write(cnes_sf, dsn= paste0("./health_facilities/shapes_in_sf_all_years_cleaned/",most_freq_year,"/cnes_sf_",most_freq_year,".gpkg"))
 
 
