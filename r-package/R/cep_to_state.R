0eaa45ca201d7bbb4d20f22abd9798bd54354187
rafapereirabr <rafa.pereira.br@gmail.com>
2021-01-10 11:18:29 -0300

    new function cep_to_state.R

--- r-package/R/cep_to_state.R
+++ r-package/R/cep_to_state.R
@@ -11,7 +11,8 @@
 #' library(geobr)
 #'
 #' uf <- cep_to_state(cep = '69900-000')
-#' # or
+#'
+#' # Or:
 #' uf <- cep_to_state(cep = 69900000)
 #'}
 #'
@@ -27,35 +28,35 @@ cep_to_state <- function(cep){
   firstdigits2 <- as.numeric(substr(cep, 1,2))
   firstdigits3 <- as.numeric(substr(cep, 1,3))
 
-  fifelse( firstdigits1 == 0,   'SP',   # Sao Paulo
-  fifelse( firstdigits1 == 1,   'SP',   # Sao Paulo
-  fifelse( firstdigits1 == 3,   'MG',   # Minas Gerais
-  fifelse( firstdigits1 == 9,   'RS',   # Rio Grande do Sul
-  fifelse( firstdigits2 ==29,   'Es',   # Espirito Santo
-  fifelse( firstdigits2 ==49,   'SE',   # Sergipe
-  fifelse( firstdigits2 ==64,   'PI',   # Piaui
-  fifelse( firstdigits2 ==65,   'MA',   # Maranhao
-  fifelse( firstdigits3 ==689,  'AP',   # Amapa
-  fifelse( firstdigits3 ==699,  'AC',   # Acre
-  fifelse( firstdigits3 ==693,  'RR',   # Roraima
-  fifelse( firstdigits2 ==77,   'TO',   # Tocantins
-  fifelse( firstdigits2 ==79,   'MS',   # Mato Grosso do Sul
-  fifelse( firstdigits2 ==78,   'MT',   # Mato Grosso
-  fifelse( firstdigits2 ==57,   'AL',   # Alagoas
-  fifelse( firstdigits2 ==58,   'PB',   # Paraiba
-  fifelse( firstdigits2 ==59,   'RN',   # Rio Grande do Norte
-  fifelse( between( firstdigits2, lower=20, upper=28),   'RJ', # Rio de Janeiro
-  fifelse( between( firstdigits2, lower=40, upper=48),   'BA', # Bahia
-  fifelse( between( firstdigits2, lower=80, upper=87),   'PR', # Parana
-  fifelse( between( firstdigits2, lower=88, upper=89),   'SC', # Santa Catarina
-  fifelse( between( firstdigits2, lower=50, upper=56),   'PE', # Pernambuco
-  fifelse( between( firstdigits2, lower=60, upper=63),   'CE', # ceara
-  fifelse( between( firstdigits2, lower=66, upper=68),   'PA', # Para
-  fifelse( between( firstdigits3, lower=690, upper=692), 'AM', # Amazonas
-  fifelse( between( firstdigits3, lower=694, upper=698), 'AM', # Amazonas
-  fifelse( between( firstdigits3, lower=700, upper=727), 'DF', # Distrito Federal
-  fifelse( between( firstdigits3, lower=730, upper=736), 'DF', # Distrito Federal
-  fifelse( between( firstdigits3, lower=768, upper=769), 'RO', # Rondônia
-  fifelse( between( firstdigits3, lower=728, upper=729), 'GO', # Goiás
-  fifelse( between( firstdigits3, lower=737, upper=767), 'GO', 'ERROR')))))))))))))))))))))))))))))))
+  ifelse( firstdigits1 == 0,   'SP',   # Sao Paulo
+  ifelse( firstdigits1 == 1,   'SP',   # Sao Paulo
+  ifelse( firstdigits1 == 3,   'MG',   # Minas Gerais
+  ifelse( firstdigits1 == 9,   'RS',   # Rio Grande do Sul
+  ifelse( firstdigits2 ==29,   'Es',   # Espirito Santo
+  ifelse( firstdigits2 ==49,   'SE',   # Sergipe
+  ifelse( firstdigits2 ==64,   'PI',   # Piaui
+  ifelse( firstdigits2 ==65,   'MA',   # Maranhao
+  ifelse( firstdigits3 ==689,  'AP',   # Amapa
+  ifelse( firstdigits3 ==699,  'AC',   # Acre
+  ifelse( firstdigits3 ==693,  'RR',   # Roraima
+  ifelse( firstdigits2 ==77,   'TO',   # Tocantins
+  ifelse( firstdigits2 ==79,   'MS',   # Mato Grosso do Sul
+  ifelse( firstdigits2 ==78,   'MT',   # Mato Grosso
+  ifelse( firstdigits2 ==57,   'AL',   # Alagoas
+  ifelse( firstdigits2 ==58,   'PB',   # Paraiba
+  ifelse( firstdigits2 ==59,   'RN',   # Rio Grande do Norte
+  ifelse( firstdigits2 >20  & firstdigits2 <28,  'RJ', # Rio de Janeiro
+  ifelse( firstdigits2 >40  & firstdigits2 <48,  'BA', # Bahia
+  ifelse( firstdigits2 >80  & firstdigits2 <87,  'PR', # Parana
+  ifelse( firstdigits2 >88  & firstdigits2 <89,  'SC', # Santa Catarina
+  ifelse( firstdigits2 >50  & firstdigits2 <56,  'PE', # Pernambuco
+  ifelse( firstdigits2 >60  & firstdigits2 <63,  'CE', # ceara
+  ifelse( firstdigits2 >66  & firstdigits2 <68,  'PA', # Para
+  ifelse( firstdigits3 >690 & firstdigits3 <692, 'AM', # Amazonas
+  ifelse( firstdigits3 >694 & firstdigits3 <698, 'AM', # Amazonas
+  ifelse( firstdigits3 >700 & firstdigits3 <727, 'DF', # Distrito Federal
+  ifelse( firstdigits3 >730 & firstdigits3 <736, 'DF', # Distrito Federal
+  ifelse( firstdigits3 >768 & firstdigits3 <769, 'RO', # Rondônia
+  ifelse( firstdigits3 >728 & firstdigits3 <729, 'GO', # Goias
+  ifelse( firstdigits3 >737 & firstdigits3 <767, 'GO', 'ERROR')))))))))))))))))))))))))))))))
 }
