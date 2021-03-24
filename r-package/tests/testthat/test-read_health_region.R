4d27fe114801ddfce16be1f9271723bd6254abaa
rafapereirabr <rafa.pereira.br@gmail.com>
2021-03-24 18:53:53 -0300

    add data of macro regions. Closes #219

--- r-package/tests/testthat/test-read_health_region.R
+++ r-package/tests/testthat/test-read_health_region.R
@@ -10,11 +10,12 @@ testthat::skip_on_cran()
 test_that("read_health_region", {
 
   # read data
-
   test_sf <- read_health_region()
+  test_sf_macro <- read_health_region(macro = T)
 
   # check sf object
   testthat::expect_true(is(test_sf, "sf"))
+  testthat::expect_true(is(test_sf_macro, "sf"))
 
   # check number of micro
   testthat::expect_equal(test_sf$code_health_region %>% length(), 438)
@@ -27,6 +28,7 @@ test_that("read_health_region", {
 test_that("read_health_region", {
 
   # Wrong year
+  testthat::expect_error(read_health_region(year=2005, macro='aaa'))
   testthat::expect_error(read_health_region(year=9999999))
   testthat::expect_error(read_health_region(year="xxx"))
   testthat::expect_error(read_health_region(year=NULL))
