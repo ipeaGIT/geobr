42052cb0a3e7b6e8fc3627c9c45fde5e946f5932
rafapereirabr <rafa.pereira.br@gmail.com>
2021-03-27 15:31:00 -0300

    minor changes to download_metadata

--- r-package/R/download_metadata.R
+++ r-package/R/download_metadata.R
@@ -8,23 +8,26 @@
 download_metadata <- function(){
 
   # create tempfile to save metadata
-  tempf <- file.path(tempdir(), "metadata.csv")
+  tempf <- file.path(tempdir(), "metadata_geobr.csv")
 
-  # check if metadata has already been downloaded
+  # IF metadata has already been downloaded
   if (file.exists(tempf)) {
-    metadata <- utils::read.csv(tempf, stringsAsFactors=F)
+
+    # skip
 
   } else {
+
+  # Download metadata
 
     #supress warnings
     oldw <- getOption("warn")
     options(warn = -1)
 
     # test server connection
-    metadata_link <- 'http://www.ipea.gov.br/geobr/metadata/metadata_gpkg.csv'
+    metadata_link <- 'https://www.ipea.gov.br/geobr/metadata/metadata_gpkg.csv'
     con <- url(metadata_link)
     t <- suppressWarnings({ try( open.connection(con, open="rt", timeout=2), silent=T)[1] })
-    if("try-error" %in% class(t)){stop('Internet connection problem. If this is not a connection problem in your network, please try geobr again in a few minutes.')}
+    if ("try-error" %in% class(t)) {stop('Internet connection problem. If this is not a connection problem in your network, please try geobr again in a few minutes.')}
     suppressWarnings({ try(close.connection(con), silent=T) })
 
     # return with warnings
@@ -32,10 +35,10 @@ download_metadata <- function(){
 
     # download it and save to metadata
     httr::GET(url= metadata_link, httr::write_disk(tempf, overwrite = T))
-    metadata <- utils::read.csv(tempf, stringsAsFactors=F)
 
     }
 
-
+  # read/return metadata
+  metadata <- utils::read.csv(tempf, stringsAsFactors=F)
   return(metadata)
   }
