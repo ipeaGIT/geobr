6908a6814c71e5f30c15248e9faa905bdebbc4fb
rafapereirabr <rafa.pereira.br@gmail.com>
2021-04-01 16:04:15 -0300

    Closes #232

---
+++ r-package/tests/testthat/test-read_urban_concentrations.R
@@ -0,0 +1,29 @@
+context("read_urban_concentrations")
+
+
+# skip tests because they take too much time
+testthat::skip_on_cran()
+# testthat::skip_on_travis()
+# skip_if(Sys.getenv("TEST_ONE") != "")
+
+
+# Reading the data -----------------------
+
+test_that("read_urban_concentrations", {
+
+  # read data and check sf object
+  test_sf <- read_urban_concentrations()
+
+  expect_true(is(test_sf, "sf"))
+
+})
+
+
+
+# ERRORS and messagens  -----------------------
+test_that("read_urban_concentrations", {
+
+  # Wrong year
+  expect_error(read_urban_concentrations(year=9999999))
+
+})
