4932cb3eb4c6e6217d328696ae5b1d7ef5496956
joaoc <joao.carabetta@gmail.com>
2021-10-11 13:31:48 -0300

    change constants datatype to float

--- python-package/geobr/constants.py
+++ python-package/geobr/constants.py
@@ -3,11 +3,11 @@ from enum import Enum
 
 class DataTypes(Enum):
 
-    code_muni = "int64"
-    code_cnes = "int64"
-    code_state = "int64"
-    year_update = "int64"
-    code_neighborhood = "int64"
-    code_subdistrict = "int64"
-    name_district = "int64"
-    geo_bater = "int64"
+    code_muni = "float"
+    code_cnes = "float"
+    code_state = "float"
+    year_update = "float"
+    code_neighborhood = "float"
+    code_subdistrict = "float"
+    code_district = "float"
+    geo_bater = "float"
