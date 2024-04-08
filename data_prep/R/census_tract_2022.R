97277d66eee0b72bef01c6d0adf50eae35ba6901
rafapereirabr <rafael.pereira.br@gmail.com>
2024-04-08 12:04:31 -0300

    download prep census tract 2022

--- data_prep/R/census_tract_2022.R
+++ data_prep/R/census_tract_2022.R
@@ -2,10 +2,43 @@ library(sf)
 library(data.table)
 library(dplyr)
 
-dest_dir <- 'C:/Users/user/Downloads/BR_Malha_Preliminar_2022/'
 
-df <- sf::st_read('C:/Users/user/Downloads/BR_Malha_Preliminar_2022/BR_Malha_Preliminar_2022.gpkg')
-saveRDS(df, 'C:/Users/user/Downloads/BR_Malha_Preliminar_2022/BR_Malha_Preliminar_2022.rds')
+year <- 2022
+
+# create dest dir
+raw_dir <- paste0('./data_raw/census_tracts/',year)
+dest_dir <- paste0('./data/census_tracts/',year)
+dir.create(raw_dir, recursive = T)
+dir.create(dest_dir, recursive = T)
+
+
+
+
+#### 0. Download original data sets from IBGE ftp -----------------
+
+if(year == 2022){
+
+  dest_file <-  download_file(file_url = ftp, dest_dir = raw_dir)
+
+}
+
+
+#### 1. unzip -----------------
+
+  temp_dir <- tempdir()
+
+  unzip(dest_file, exdir = temp_dir)
+
+  local_file <- unzip_fun(dest_file)
+
+
+
+df <- sf::st_read('./data_raw/census_tracts/2022/BR_Malha_Preliminar_2022.gpkg')
+saveRDS(df, './data_raw/census_tracts/2022/BR_Malha_Preliminar_2022.rds')
+
+
+
+#### 1. clean and save data -----------------
 
 df$AREA_KM2 <- NULL
 
@@ -51,13 +84,21 @@ temp_sf <- use_encoding_utf8(temp_sf)
 
 # Harmonize spatial projection CRS, using SIRGAS 2000 epsg (SRID): 4674
 temp_sf <- harmonize_projection(temp_sf)
+gc()
 
-save_state <- function(code_uf){ # code_uf <- 11
+
+# harmonize and save
+
+save_state <- function(code_uf){ # code_uf <- 33
 
     temp_sf2 <- subset(temp_sf, code_state == code_uf)
+<<<<<<< HEAD
+    # temp_sf2 <- subset(temp_sf2, code_muni == '3304557')
+=======
 
    # temp_sf2 <- subset(temp_sf2, code_muni == '3304557')
 
+>>>>>>> e8fd7687ce69803eb31daab82f51a60a25f91525
 
     # convert to MULTIPOLYGON
     temp_sf2 <- to_multipolygon(temp_sf2)
@@ -66,8 +107,8 @@ save_state <- function(code_uf){ # code_uf <- 11
     temp_sf_simplified <- simplify_temp_sf(temp_sf2, tolerance = 10)
 
     # Save cleaned sf in the cleaned directory
-  #  sf::st_write(temp_sf2, paste0(dest_dir, code_uf,".gpkg") )
-    sf::st_write(temp_sf_simplified, paste0(dest_dir, code_uf,"_simplified.gpkg"))
+    sf::st_write(temp_sf2, paste0(dest_dir,'/', code_uf,'census_tract_', year, '.gpkg'))
+    sf::st_write(temp_sf_simplified, paste0(dest_dir,'/', code_uf,'census_tract_', year, '_simplified.gpkg'))
 
   }
 
@@ -77,10 +118,10 @@ all_states <- unique(temp_sf$code_state)
 # Apply function to save the data
 gc(reset = T)
 
-tictoc::tic()
-future::plan(strategy = 'multisession')
-furrr::future_map(.x=all_states, .f=save_state, .progress = T)
-tictoc::toc()
+# tictoc::tic()
+# future::plan(strategy = 'multisession')
+# furrr::future_map(.x=all_states, .f=save_state, .progress = T)
+# tictoc::toc()
 
 
 tictoc::tic()
