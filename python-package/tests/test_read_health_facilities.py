a07c82ad302a2aec9364186450d6c1e3a004e8b8
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2024-04-24 00:34:42 -0300

    Update tests and error messages

--- python-package/tests/test_read_health_facilities.py
+++ python-package/tests/test_read_health_facilities.py
@@ -9,7 +9,13 @@ def test_read_health_facilities():
 
     assert isinstance(df, gpd.geodataframe.GeoDataFrame)
 
-    assert len(df) == 360177
+    assert len(df) == 517629
 
     with pytest.raises(Exception):
         read_health_facilities(year=9999999)
+
+    with pytest.raises(Exception):
+        read_health_facilities(year="banana")
+
+    with pytest.raises(Exception):
+        read_health_facilities(verbose="banana")
