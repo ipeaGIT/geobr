f1c60f8fc569156b91a3dcd75eaad2e3969f56ff
rafapereirabr <rafa.pereira.br@gmail.com>
2026-04-17 00:26:11 -0300

    read_urban_area v2.0 R

--- r-package/tests/testthat/test-read_urban_area.R
+++ r-package/tests/testthat/test-read_urban_area.R
@@ -11,27 +11,24 @@ skip_if(Sys.getenv("TEST_ONE") != "")
 test_that("read_urban_area", {
 
   # read data and check sf object
-  test_sf <- read_urban_area()
-  expect_true(is(test_sf, "sf"))
-
   test_sf <- read_urban_area(year = 2015)
   expect_true(is(test_sf, "sf"))
 
   # filter state code
-  test_sf <- read_urban_area(code_state = 33)
+  test_sf <- read_urban_area(code_state = 33,year = 2015)
   expect_true(is(test_sf, "sf"))
   expect_true(33 %in% unique(test_sf$code_state))
 
-  test_sf <- read_urban_area(code_state = c(33, 35))
+  test_sf <- read_urban_area(code_state = c(33, 35), year = 2015)
   expect_true(is(test_sf, "sf"))
   expect_true(all(c(33, 35) %in% unique(test_sf$code_state)))
 
   # filter state abbrev
-  test_sf <- read_urban_area(code_state = 'RJ')
+  test_sf <- read_urban_area(code_state = 'RJ', year = 2015)
   expect_true(is(test_sf, "sf"))
   expect_true('RJ' %in% unique(test_sf$abbrev_state))
 
-  test_sf <- read_urban_area(code_state = c('RJ', 'SP'))
+  test_sf <- read_urban_area(code_state = c('RJ', 'SP'), year = 2015)
   expect_true(is(test_sf, "sf"))
   expect_true(all(c('RJ', 'SP') %in% unique(test_sf$abbrev_state)))
 
@@ -43,11 +40,12 @@ test_that("read_urban_area", {
 test_that("read_urban_area", {
 
   # Wrong year
+  expect_error(read_urban_area())
   expect_error(read_urban_area(year=9999999))
 
   # filter state
-  expect_error(read_urban_area(code_state = c('RJ', 33)))
-  expect_error(read_urban_area(code_state = 'banana'))
-  expect_error(read_urban_area(code_state = 999999999))
+  expect_error(read_urban_area(code_state = c('RJ', 33), year = 2015))
+  expect_error(read_urban_area(code_state = 'banana', year = 2015))
+  expect_error(read_urban_area(code_state = 999999999, year = 2015))
 
 })
