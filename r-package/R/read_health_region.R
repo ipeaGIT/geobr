29421daa31acdb22a444fd2bb064f071cc5cf6fa
rafapereirabr <rafa.pereira.br@gmail.com>
2026-04-25 16:20:13 -0300

    read_health_region

--- r-package/R/read_health_region.R
+++ r-package/R/read_health_region.R
@@ -1,16 +1,19 @@
 #' Download spatial data of Brazilian health regions and health macro regions
 #'
 #' @description
-#' Health regions are used to guide the the regional and state planning of health services.
-#' Macro health regions, in particular, are used to guide the planning of high complexity
-#' health services. These services involve larger economics of scale and are concentrated in
-#' few municipalities because they are generally more technology intensive, costly and face
-#' shortages of specialized professionals. A macro region comprises one or more health regions.
+#' Health regions are used to guide the the regional and state planning of health
+#' services. Macro health regions, in particular, are used to guide the planning
+#' of high complexity #' health services. These services involve larger economics
+#' of scale and are concentrated in few municipalities because they are generally
+#' more technology intensive, costly and face shortages of specialized
+#' professionals. A macro region comprises one or more health regions.
 #'
 #' @template year
+#' @template code_state
 #' @param macro Logic. If `FALSE` (default), the function downloads health
 #'        regions data. If `TRUE`, the function downloads macro regions data.
 #' @template simplified
+#' @template as_sf
 #' @template showProgress
 #' @template cache
 #' @template verbose
@@ -22,14 +25,16 @@
 #'
 #' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
 #' # Read all health regions for a given year
-#' hr <- read_health_region( year=2013 )
+#' hr <- read_health_region(year = 2024 )
 #'
 #' # Read all macro health regions
-#' mhr <- read_health_region( year=2013, macro =TRUE)
+#' mhr <- read_health_region(year=2024, macro =TRUE)
 #'
-read_health_region <- function(year = NULL,
+read_health_region <- function(year,
+                               code_state = "all",
                                macro = FALSE,
                                simplified = TRUE,
+                               as_sf = TRUE,
                                showProgress = TRUE,
                                cache = TRUE,
                                verbose = TRUE){
@@ -41,7 +46,7 @@ read_health_region <- function(year = NULL,
 
   # Get metadata
   temp_meta <- select_metadata(
-    geography = temp_geo,
+    geography = "healthregions",
     year = year,
     simplified = simplified,
     verbose = verbose
@@ -60,9 +65,18 @@ read_health_region <- function(year = NULL,
   # check if download failed
   if (is.null(temp_arrw)) { return(invisible(NULL)) }
 
+  # FILTER
+  temp_arrw <- filter_arrw(temp_arrw, code = code_state)
+
   # convert to sf
   output <- convert_arrow2sf(temp_arrw, as_sf)
 
   return(output)
 
+# a <- duckspatial::ddbs_union_agg(x = output, by = "code_health_region") |>
+#   duckspatial::ddbs_collect() |>
+#   sfheaders::sf_remove_holes()
+# mapview::mapview(a)
+
 }
+
