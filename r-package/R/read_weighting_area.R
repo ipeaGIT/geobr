50338249e2a4c43c770d6c52eaf266f6964fcaeb
rafapereirabr <rafa.pereira.br@gmail.com>
2026-05-05 11:33:53 -0300

    read_weighting_area v2.0 R

--- r-package/R/read_weighting_area.R
+++ r-package/R/read_weighting_area.R
@@ -6,8 +6,9 @@
 #' @template year
 #' @param code_weighting The 7-digit code of a Municipality. If the two-digit code
 #' or a two-letter uppercase abbreviation of a state is passed, (e.g. 33 or "RJ")
-#' the function will load all weighting areas of that state. If `code_weighting="all"`,
-#' all weighting areas of the country are loaded.
+#' the function will load all weighting areas of that state. If
+#' `code_weighting="all"` (the default), all weighting areas of the country are
+#' loaded.
 #' @template simplified
 #' @template as_sf
 #' @template showProgress
@@ -21,22 +22,31 @@
 #'
 #' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
 #' # Read specific weighting area at a given year
-#' w <- read_weighting_area(code_weighting=5201108005004, year=2010)
+#' w <- read_weighting_area(
+#'   code_weighting = 5201108005004,
+#'   year = 2010
+#'   )
 #'
 #' # Read all weighting areas of a state at a given year
-#' w <- read_weighting_area(code_weighting=53, year=2010) # or
-#' w <- read_weighting_area(code_weighting="DF", year=2010)
-#' plot(w)
+#' w <- read_weighting_area(
+#'   code_weighting = "DF",
+#'   year = 2010
+#'   )
 #'
 #' # Read all weighting areas of a municipality at a given year
-#' w <- read_weighting_area(code_weighting=5201108, year=2010)
-#' plot(w)
+#' w <- read_weighting_area(
+#'   code_weighting = 5201108,
+#'   year = 2010
+#'   )
 #'
 #' # Read all weighting areas of the country at a given year
-#' w <- read_weighting_area(code_weighting="all", year=2010)
+#' w <- read_weighting_area(
+#'   code_weighting = "all",
+#'   year = 2010
+#'   )
 #'
 read_weighting_area <- function(year,
-                                code_weighting,
+                                code_weighting = "all",
                                 simplified = TRUE,
                                 as_sf = TRUE,
                                 showProgress = TRUE,
