7a9e2da10d1260bfe78dc066bd3dbbb45e615308
rafapereirabr <rafa.pereira.br@gmail.com>
2026-05-07 22:57:17 -0300

    update docs read_schools

--- r-package/R/read_schools.R
+++ r-package/R/read_schools.R
@@ -1,15 +1,29 @@
 #' Download geolocated data of schools
 #'
 #' @description
-#' Data comes from the School Census collected by INEP, the National Institute
-#' for Educational Studies and Research Anisio Teixeira. The date of the last
-#' data update is registered in the database in the column 'date_update'. These
-#' data uses Geodetic reference system "SIRGAS2000" and CRS(4674). The coordinates
-#' of each school if collected by INEP. Periodically the coordinates are revised
-#' with the objective of improving the quality of the data. More information
-#' available at \url{https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/inep-data/catalogo-de-escolas/}
+#' Data comes from the Catalogue of Schools Census, organized by the National
+#' Institute for Educational Studies and Research Anisio Teixeira (INEP). The
+#' date of the last data update is registered in the database in the column
+#' `"date_update"`. More information available at \url{https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/inep-data/catalogo-de-escolas/}.
+#' The spatial coordinates used in geobr are a combination of the coordinates
+#' produced by the original data producer and the coordinates found via geocoding
+#' with the geocodebr package \url{https://CRAN.R-project.org/package=geocodebr}.
+#' Whenever the distance between the coordinates from both sources is smaller than
+#' 800 meters, geobr uses coordinates from the data producer. When the distance
+#' between the two sources is greater than 800 meters and the results from
+#' geocodebr have a precision level finer than 800 meters, geobr uses the
+#' coordinates from geocodebr. When the coordinates from the original source are
+#' missing, geobr also uses geocodebr coordinates, regardless of precision level.
+#' The source of the spatial coordinates used in each observation is registered
+#' in the data in a specific column `coords_source`. Additional columns
+#' indicating the precision level of geocodebr geocoding are also included in
+#' the data.
+
+#'
 #'
 #' @template year
+#' @template code_muni
+#' @template as_sf
 #' @template showProgress
 #' @template cache
 #' @template verbose
@@ -22,7 +36,9 @@
 #' # Read all schools in the country
 #' s <- read_schools( year = 2020)
 #'
-read_schools <- function(year = NULL,
+read_schools <- function(year,
+                         code_muni = "all",
+                         as_sf = TRUE,
                          showProgress = TRUE,
                          cache = TRUE,
                          verbose = TRUE){
@@ -31,7 +47,7 @@ read_schools <- function(year = NULL,
   temp_meta <- select_metadata(
     geography="schools",
     year = year,
-    simplified = simplified,
+    simplified = FALSE,
     verbose = verbose
   )
 
@@ -47,6 +63,9 @@ read_schools <- function(year = NULL,
 
   # check if download failed
   if (is.null(temp_arrw)) { return(invisible(NULL)) }
+
+  # FILTER
+  temp_arrw <- filter_arrw(temp_arrw, code = code_muni)
 
   # convert to sf
   output <- convert_arrow2sf(temp_arrw, as_sf)
