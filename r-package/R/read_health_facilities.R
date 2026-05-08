b40e3b3f37ce05e9e1bacd6d26e7a924d4fdbc68
rafapereirabr <rafa.pereira.br@gmail.com>
2026-05-07 21:33:43 -0300

    update docs of read_health_facilities

--- r-package/R/read_health_facilities.R
+++ r-package/R/read_health_facilities.R
@@ -1,27 +1,25 @@
 #' Download geolocated data of health facilities
 #'
 #' @description
-#' Data comes from the National Registry of Healthcare facilities (Cadastro
+#' Data comes from the National Registry of Health Facilities (Cadastro
 #' Nacional de Estabelecimentos de Saude - CNES), originally collected by the
-#' Brazilian Ministry of Health. According to the Ministry of Health: "The
-#' coordinates of each facility were obtained by CNES and validated by means of
-#' space operations. These operations verify if the point is in the municipality,
-#' considering a radius of 5,000 meters. When the coordinate is not correct,
-#' further searches are done in other systems of the Ministry of Health and in
-#' web services like Google Maps. Finally, if the coordinates have been correctly
-#' obtained in this process, the coordinates of the municipal head office are
-#' used." Whenever possible, geobr uses the spatial coordinates from CNES. Only
-#' when CNES coordinates are not available, then geobr uses the spatial
-#' coordinates found via geocoding with the geocodebr package \url{https://CRAN.R-project.org/package=geocodebr}.
-#' The source of the spatial coordinates is registered in the data in a specific
-#' column `coords_source`. Periodically the coordinates are revised with the
-#' objective of improving the quality of the data. The date of the last data
-#' update is registered in the data in the columns `date_update` and
-#' `year_update`. More information in the CNES data set available at \url{https://dados.gov.br/}.
-#' These data use Geodetic reference system "SIRGAS2000" and CRS(4674).
+#' Brazilian Ministry of Health.
+#' The spatial coordinates used in geobr are a combination of the coordinates
+#' produced by the original data producer and the coordinates found via geocoding
+#' with the geocodebr package \url{https://CRAN.R-project.org/package=geocodebr}.
+#' Whenever the distance between the coordinates from both sources is smaller than
+#' 800 meters, geobr uses coordinates from the data producer. When the distance
+#' between the two sources is greater than 800 meters and the results from
+#' geocodebr have a precision level finer than 800 meters, geobr uses the
+#' coordinates from geocodebr. When the coordinates from the original source are
+#' missing, geobr also uses geocodebr coordinates, regardless of precision level.
+#' The source of the spatial coordinates used in each observation is registered
+#' in the data in a specific column `coords_source`. Additional columns
+#' indicating the precision level of geocodebr geocoding are also included in
+#' the data.
 #'
 #' @template date
-#' @template code_state
+#' @template code_muni
 #' @template as_sf
 #' @template showProgress
 #' @template cache
@@ -30,20 +28,19 @@
 #' @return An `"sf" "data.frame"` OR an `ArrowObject`
 #'
 #' @export
-#' @family area functions
 #'
 #' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
 #' # Read health facilities of a given state
 #' h <- read_health_facilities(
 #'   date = 202604,
-#'   code_state = "PA"
+#'   code_muni = "PA"
 #'   )
 #'
 #' # Read all health facilities of the whole country
 #' h <- read_health_facilities(date = 202604)
 #'
 read_health_facilities <- function(date,
-                                   code_state,
+                                   code_muni = "all",
                                    as_sf = TRUE,
                                    showProgress = TRUE,
                                    cache = TRUE,
@@ -71,7 +68,7 @@ read_health_facilities <- function(date,
   if (is.null(temp_arrw)) { return(invisible(NULL)) }
 
   # FILTER
-  temp_arrw <- filter_arrw(temp_arrw, code = code_state)
+  temp_arrw <- filter_arrw(temp_arrw, code = code_muni)
 
   # convert to sf
   output <- convert_arrow2sf(temp_arrw, as_sf)
