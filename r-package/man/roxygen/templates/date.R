0f43a58ce70d6eab24df6943dac23f42fffd48e2
rafapereirabr <rafa.pereira.br@gmail.com>
2026-06-21 16:02:22 -0300

    docs

--- r-package/man/roxygen/templates/date.R
+++ r-package/man/roxygen/templates/date.R
@@ -1,2 +1,1 @@
-#' @param date Numeric. Date of the data in YYYYMM format. It defaults to `NULL`
-#'        and reads the data from the latest date available.
+#' @param date Numeric. Date of the data in `YYYYMM` format.
